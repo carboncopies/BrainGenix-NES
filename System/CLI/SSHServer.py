c68be7ae900650741caff44a9918c85570fdd2c9
Data_Crystals <thomasl20@icloud.com>
2021-01-18 11:14:15 -0800

    Create Blank Files For SSH Server

---
+++ System/CLI/SSHServer.py
@@ -0,0 +1,12 @@
+###########################################################
+## This file is part of the BrainGenix Simulation System ##
+###########################################################
+
+import paramiko
+
+'''
+Name: SSHServer
+Description: This file runs the actual SSH server.
+Date-Created: 2021-01-17
+'''
+
