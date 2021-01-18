245554c3c5ec0f2683163e269cf9e63d4da9a718
Data_Crystals <thomasl20@icloud.com>
2021-01-18 11:14:40 -0800

    Add Blank File For Database Interface

---
+++ System/Database/DatabaseInterface.py
@@ -0,0 +1,11 @@
+###########################################################
+## This file is part of the BrainGenix Simulation System ##
+###########################################################
+
+'''
+Name: Database Interface
+Description: This file creates an interface from BrainGenix to mySQL.
+Date-Created: 2021-01-17
+'''
+
+
