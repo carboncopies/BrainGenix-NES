1454d6c19d0e0bf1b03d90f791a94a336bb99cfe
Data_Crystals <thomasl20@icloud.com>
2021-01-29 22:28:43 -0800

    Update CLI To Support Arguments

--- System/CLI/CommandParser.py
+++ System/CLI/CommandParser.py
@@ -16,8 +16,19 @@ def TraceCall(Command:str): # Traces A Command To The Right Function Call #
     *Please Don't Call This Unless You Know What You're Doing!*
     '''
 
+    # Get Args #
+    ArgumentList = Command.split(' ')
+
     # Parse Into List Of Calls #
-    NameList = Command.split('.')
+    NameList = ArgumentList[0].split('.')
+    del ArgumentList[0]
+
+    # Format Args #
+    ArgDict = {}
+    for ArgumentIndex in range(int(len(ArgumentList)/2)):
+        Kwarg = ArgumentList[ArgumentIndex*2]
+        Value = ArgumentList[ArgumentIndex*2 + 1]
+        ArgDict.update({Kwarg : Value})
 
     # Return The List #
-    return NameList
+    return NameList, ArgDict
