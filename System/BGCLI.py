08e461584a7bdfe5d168f9ef99e947633a814c54
Data_Crystals <thomasl20@icloud.com>
2021-01-29 18:47:50 -0800

    Update BGCLI Client

--- System/BGCLI.py
+++ System/BGCLI.py
@@ -5,6 +5,8 @@
 import threading
 import time
 import uuid
+import readline
+import atexit
 
 from kazoo.client import KazooClient
 
@@ -21,22 +23,61 @@ class Client(): # Client For BrainGenix System #
         # Connect To ZK #
         self.ZookeeperConnection = KazooClient(hosts=ZKAddress)
         self.ZookeeperConnection.start()
+        self.Username = Username
+        self.Host = ZKAddress
+        print('Initializing Connection')
 
         self.UUID = str(uuid.uuid1())
 
         # Create A Connection zNode #
         self.ConnectionNode = f'/BrainGenix/CLI/{self.UUID}'
+        print('Connected to Zookeeper')
         self.ZookeeperConnection.create(self.ConnectionNode, ephemeral=True)
+        print('Created Epehemeral zNode')
 
         # Write The Authentication Information To The System #
         AuthInfo = f'{Username}\n{Password}'
         AuthInfo = AuthInfo.encode()
         self.ZookeeperConnection.set(self.ConnectionNode, AuthInfo)
+        print('Wrote Authentication Information')
+
+        # Block Until Auth Passes #
+        while self.ZookeeperConnection.get(self.ConnectionNode)[0] == AuthInfo:
+            time.sleep(0.05)
+        print('Connection Picked up by Leader')
 
     def Main(self): # Main Loop #
 
+        # Loop For Connection #
         while True:
 
-            pass
+            # Get Command Input From User #
+            Command = input(f'[BrainGenix: {self.Host}(Connected:{self.Username})]: ')
 
-test = Client('tliao', '123456', '127.0.0.1:2181')
+            if Command == '':
+                pass
+            elif Command == 'exit':
+                exit()
+            else:
+                self.ZookeeperConnection.set(self.ConnectionNode, Command.encode())
+
+                # Block Until Command Finished #
+                while self.ZookeeperConnection.get(self.ConnectionNode)[0] == Command.encode():
+                    time.sleep(0.05)
+                
+                # Handle Output #
+                CommandOutput = self.ZookeeperConnection.get(self.ConnectionNode)[0].decode()
+
+                if CommandOutput != '':
+                    print(CommandOutput)
+                else:
+                    pass
+
+# Set Exit Params #
+@atexit.register
+def Disconnect():
+    CLI.ZookeeperConnection.delete(CLI.ConnectionNode)
+
+# Instantiate The Client #
+CLI = Client('root', 'turing', '127.0.0.1:2181')
+CLI.Main()
