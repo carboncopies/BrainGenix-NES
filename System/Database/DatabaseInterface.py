dd25eb0d8a006d9582ade96014f13ae0d4c0d4b5
Prishita Ray <prishita.r85@gmail.com>
2021-02-07 01:32:13 +0530

    Update DatabaseInterface.py

--- System/Database/DatabaseInterface.py
+++ System/Database/DatabaseInterface.py
@@ -4,6 +4,7 @@
 
 #import mysql.connector
 import pymysql
+import encryption
 
 '''
 Name: Database Interface
@@ -20,6 +21,10 @@ class DBInterface(): # Interface to MySQL database #
 
         # Establish DB Connection #
         self.DBConnection = pymysql.connect(host=Host, user=Username, password=Password, db=Database)
+        
+        #Encrypt the host befor logging
+        Host= encryption.encrypt(Logger:object, Host:str)
+        
         self.Logger.Log(f'Connected To DB At {Host}')
         
         self.DBConnection.close()
