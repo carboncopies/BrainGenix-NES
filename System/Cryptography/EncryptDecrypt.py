6afe11cd47992863ccca8943d28396b14f554449
Prishita Ray <prishita.r85@gmail.com>
2021-02-08 16:00:51 +0530

    Correct Indentation Levels

--- System/Cryptography/EncryptDecrypt.py
+++ System/Cryptography/EncryptDecrypt.py
@@ -12,56 +12,53 @@ Description: This file performs encryption and decryption on the BrainGenix Comm
 Date-Created: 2021-02-07
 '''
 
+# Initialize The Class #
 class EncryptDecrypt:
-	
-	def __init__(self, PrimeLength:int=2048): # Initialize The Class #
 
-        # Generate A Prime Nubmer Of Specified Length #
-		self.Prime= hex(number.getPrime(PrimeLength)) 
+	def __init__(self, PrimeLength:int=2048):# Generate A Prime Nubmer Of Specified Length #
+
+            self.Prime= hex(number.getPrime(PrimeLength)) 
 
 
 	def GenerateSecret(self, PrivateKey, PublicKey): # Generates A New Secret #
+            # Gen Secret #
+            Secret = pow(int(PublicKey), int(PrivateKey), self.Prime)
+            SecretBytes = str(Secret)
 
-		# Gen Secret #
-		Secret = pow(int(PublicKey), int(PrivateKey), self.Prime)
-		SecretBytes = str(Secret)
+            # Generate Hash Key Using SHA256 #
+            Key = SHA256.new()
+            Key.update(bytes(SecretBytes))
+            SecretKey = Key.hexdigest()
 
-	    # Generate Hash Key Using SHA256 #
-	    Key = SHA256.new()
-	    Key.update(bytes(SecretBytes))
-	   	SecretKey = Key.hexdigest()
-	    
-		# Return Secret Key #
-		return SecretKey
+            # Return Secret Key #
+            return SecretKey
 
 
 	def Encrypt(self, Logger:object, Command:str):
 
-		# Load The Pub/Private Keys #
-		PublicKey, PrivateKey = ReadKeys(Logger)
-		SecretKey = GenerateSecret(PublicKey, PrivateKey)
+            # Load The Pub/Private Keys #
+            PublicKey, PrivateKey = ReadKeys(Logger)
+            SecretKey = GenerateSecret(PublicKey, PrivateKey)
 
-		## CAN WE REPLACE THIS WITH AES 256 ##
-		EncryptedCommand = ""
+            EncryptedCommand = ""
 
-		for c in Command:
-			EncryptedCommand += chr(ord(c)+ SecretKey)
+            for c in Command:
+                EncryptedCommand += chr(ord(c)+ SecretKey)
 
-
-		# Return The Encrypted Command #
-		return EncryptedCommand
+            # Return The Encrypted Command #
+            return EncryptedCommand
 	
 
 	def Decrypt(self, Logger:object, encCommand:str):
 
-	    PublicKey, PrivateKey = ReadKeys(Logger)
-	    SecretKey = GenerateSecret(PublicKey, PrivateKey)
+            PublicKey, PrivateKey = ReadKeys(Logger)
+            SecretKey = GenerateSecret(PublicKey, PrivateKey)
 
-		## CAN WE REPLACE THIS WITH AES 256 ##
-		DecryptedCommand = ""
-			
-		for c in encCommand:
-			DecryptedCommand += chr(ord(c)-SecretKey)
-		
-		# Return The Decrypted Command #
-		return DecryptedCommand
+            ## CAN WE REPLACE THIS WITH AES 256 ##
+            DecryptedCommand = ""
+
+            for c in encCommand:
+                DecryptedCommand += chr(ord(c)-SecretKey)
+
+            # Return The Decrypted Command #
+            return DecryptedCommand
