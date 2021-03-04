fd502d087a9dddeec370b3e98b4682f6a8f90633
datacrystals <tliao@carboncopies.org>
2021-03-03 17:10:00 -0800

    Resolve False "Hardcoded Secret" Trigger

--- System/APIServer.py
+++ System/APIServer.py
@@ -18,16 +18,7 @@ from Zookeeper.Zookeeper import ZK
 
 #https://realpython.com/python-https/
 #Client -> Proxy(This File, Hosts API) -> Zookeeper -> Leader -> Follower(s)
-# IS THERE A BETTER WAY TO DO THIS THAT DOESN'T NEED ALL THESE STEPS? I'M NOT SURE OF ONE #
 
-# from flask import Flask
-
-# SECRET_MESSAGE = "fluffy tail"
-# app = Flask(__name__)
-
-# @app.route("/")
-# def get_secret_message():
-#     return SECRET_MESSAGE
 
 
 # Set Version Information
