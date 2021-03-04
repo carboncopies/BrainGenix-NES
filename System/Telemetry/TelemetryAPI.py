afd95235ac7f1ec3f89fc77f6c20899dd261dc7e
datacrystals <tliao@carboncopies.org>
2021-03-04 13:02:46 -0800

    Resolve C0303 In TelemetryAPI

--- System/Telemetry/TelemetryAPI.py
+++ System/Telemetry/TelemetryAPI.py
@@ -8,7 +8,7 @@ Description: This file is used to connect the system's API to the leader.
 Date-Created: 2021-03-03
 '''
 
-class TelemetryAPI(): # Exposes Telemetry Functions To The Leader # 
+class TelemetryAPI(): # Exposes Telemetry Functions To The Leader #
 
     def __init__(self, Zookeeper, Logger, LeaderAttributes): # Initializes the System #
 
