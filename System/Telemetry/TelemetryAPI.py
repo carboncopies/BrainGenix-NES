7d4974d347dbd2389d5fbfcfe7c120a1c0c57e7d
datacrystals <tliao@carboncopies.org>
2021-03-04 17:54:54 -0800

    Begin Adding Functions For The API

--- System/Telemetry/TelemetryAPI.py
+++ System/Telemetry/TelemetryAPI.py
@@ -19,3 +19,45 @@ class TelemetryAPI(): # Exposes Telemetry Functions To The Leader #
 
         # Log Message #
         self.Logger.Log('Starting Telemetry API Handler')
+
+
+
+    def ReadZKCommands(self): # Reads the command and creates a response # 
+
+        pass
+
+
+    def GetClusterSize(self): # Gets the cluster's size #
+        
+        # Get The Number Of Currently Connected Nodes In the System #
+        return len(self.LeaderAttributes.Info)
+
+
+    def GetOperatingSystem(self, NodeName) # Gets The Operating System Name #
+
+        # Get The Info # 
+        NodeJSON = self.LeaderAttributes.Info[NodeName]
+        OSName = NodeJSON['OperatingSystemName']
+        
+        # Return The Information # 
+        return OSName
+
+    
+    def GetNodeHostname(self, NodeName): # Gets The Node's Hostname # 
+
+        # Get The Info # 
+        NodeJSON = self.LeaderAttributes.Info[NodeName]
+        NodeName = NodeJSON['NodeName']
+        
+        # Return The Information # 
+        return NodeName
+
+
+    def GetOperatingSystemRelease(self, NodeName) # Gets The Operating System Version #
+
+        # Get The Info # 
+        NodeJSON = self.LeaderAttributes.Info[NodeName]
+        Info = NodeJSON['OperatingSystemRelease']
+        
+        # Return The Information # 
+        return Info
