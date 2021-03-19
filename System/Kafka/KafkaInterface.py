b6e06c165d697e2af9f088eeae868d70f9579b56
datacrystals <tliao@carboncopies.org>
2021-03-19 14:57:14 -0700

    Create Kafka Interface Framework

--- System/Kafka/KafkaInterface.py
+++ System/Kafka/KafkaInterface.py
@@ -9,27 +9,45 @@ Date-Created: 2021-03-18
 '''
 
 import kafka
-
-#from kafka import KafkaConsumer
-#from kafka import KafkaProducer
-
-
-# # Producer = KafkaProducer(bootstrap_servers='localhost:9092')
-# # for z in range(100):
-# #     Producer.send('topicname', f'{z}'.encode('utf-8'))
-
-# Consumer = KafkaConsumer(bootstrap_servers='localhost:9092')
-# #Consumer.subscribe('topicname')
-
-# print(Consumer)
-# for message in Consumer:
-
-#     print(message)
-
+import time
+import random
 
 from kafka import KafkaConsumer
-from kafka import TopicPartition
-consumer = KafkaConsumer(bootstrap_servers='localhost:9092')
-consumer.assign([TopicPartition('topicname', 0)])
-for msg in consumer:
-    print(msg)
+from kafka import KafkaProducer
+from kafka.admin import KafkaAdminClient
+from kafka.admin import NewTopic
+
+
+class KafkaInterface(): # Defines a class used to communicate with Kafka from BrainGenix #
+
+
+    def __init__(self, Host):
+
+        # Store Host For Later Use #
+        self.Host = Host
+
+        # Initialize Kafka Interfaces #
+        self.KafkaProducer = KafkaProducer(bootstrap_servers = Host)
+        self.KafkaConsumer = KafkaConsumer(bootstrap_servers = Host)
+
+
+    def CreateTopics(self, TopicNames): # Creates a topic or Queue
+
+        # Connect To Kafka #
+        AdminClient = KafkaAdminClient(bootstrap_servers=self.Host)
+
+        # Create Topic List #
+        TopicsList = []
+        for TopicName in TopicNames:
+            TopicObject = NewTopic(TopicName, num_partitions=1, replication_factor=1)
+            TopicsList.append(TopicObject)
+
+        # Create Topics #
+        AdminClient.create_topics(new_topics=TopicsList, validate_only=False)
+
+
+
+
+
+K = KafkaInterface('127.0.0.1:9092')
+K.CreateTopics(['test1','test2'])
