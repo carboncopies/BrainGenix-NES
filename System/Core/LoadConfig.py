a9266d64584cb92d3e1af0799b1063b00809bb08
datacrystals <tliao@carboncopies.org>
2021-03-21 17:15:54 -0700

    Update Config Loader

--- System/Core/LoadConfig.py
+++ System/Core/LoadConfig.py
@@ -11,7 +11,7 @@ Date-Created: 2020-12-18
 '''
 
 
-def LoadConfig(ConfigFilePath):
+def LoadLoggerConfig(ConfigFilePath):
 
     '''
     This function loads the config from a file path.
@@ -28,26 +28,84 @@ def LoadConfig(ConfigFilePath):
 
 
     # Extract the important values from the dictionary and return them to the main system #
-    AddonsPath = str(ConfigFileDictionary.get('AddonsPath'))
     LogPath = str(ConfigFileDictionary.get('LogPath'))
     PrintLogText = bool(ConfigFileDictionary.get('PrintLogText'))
     LogBufferLength = int(ConfigFileDictionary.get('LogBufferLength'))
     LogFileLinesBeforeSplit = int(ConfigFileDictionary.get('LogFileLinesBeforeSplit'))
     EnableGzip = bool(ConfigFileDictionary.get('gzipLogs'))
-    ZKHost = str(ConfigFileDictionary.get('ZKHost'))
-    ZKPort = str(ConfigFileDictionary.get('ZKPort'))
 
-    ZKHost += f':{ZKPort}'
+    # Return the values #
+    return LogPath, LogBufferLength, PrintLogText, LogFileLinesBeforeSplit, EnableGzip
 
-    KafkaHost = str(ConfigFileDictionary.get('KafkaHost'))
-    KafkaPort = str(ConfigFileDictionary.get('KafkaPort'))
 
-    KafkaHost += f':{KafkaPort}'
 
+def LoadDatabaseConfig(ConfigFilePath):
+
+    '''
+    This function loads the config from a file path.
+    It's designed to read a specific set of values from the path, so it doesn't just return a dictionary value.
+    This loader is not designed to be used except as the initial config loader, as it only reads a few select parameters.
+    *Please don't call this function unless you know what you're doing!*
+    '''
+
+    # First Read the contents of the file and parse it #
+    with open(ConfigFilePath, 'r') as File:
+        ConfigFileContents = File.read()
+
+    ConfigFileDictionary = yaml.safe_load(ConfigFileContents)
+
+    # Extract Values From Dictionary #
     DBUname = str(ConfigFileDictionary.get('DatabaseUsername'))
     DBPasswd = str(ConfigFileDictionary.get('DatabasePassword'))
     DBHost = str(ConfigFileDictionary.get('DatabaseHost'))
     DBName = str(ConfigFileDictionary.get('DatabaseName'))
 
-    # Return the values #
-    return AddonsPath, LogPath, LogBufferLength, PrintLogText, LogFileLinesBeforeSplit, EnableGzip, ZKHost, DBUname, DBPasswd, DBHost, DBName, KafkaHost
+    return DBUname, DBPasswd, DBHost, DBName
+
+
+def LoadZookeeperConfig(ConfigFilePath):
+
+    '''
+    This function loads the config from a file path.
+    It's designed to read a specific set of values from the path, so it doesn't just return a dictionary value.
+    This loader is not designed to be used except as the initial config loader, as it only reads a few select parameters.
+    *Please don't call this function unless you know what you're doing!*
+    '''
+
+    # First Read the contents of the file and parse it #
+    with open(ConfigFilePath, 'r') as File:
+        ConfigFileContents = File.read()
+
+    ConfigFileDictionary = yaml.safe_load(ConfigFileContents)
+
+    # Extract Values From Dictionary #
+    ZKHost = str(ConfigFileDictionary.get('ZKHost'))
+    ZKPort = str(ConfigFileDictionary.get('ZKPort'))
+
+    ZKHost += f':{ZKPort}'
+
+    return ZKHost
+
+
+def LoadKafkaConfig(ConfigFilePath):
+
+    '''
+    This function loads the config from a file path.
+    It's designed to read a specific set of values from the path, so it doesn't just return a dictionary value.
+    This loader is not designed to be used except as the initial config loader, as it only reads a few select parameters.
+    *Please don't call this function unless you know what you're doing!*
+    '''
+
+    # First Read the contents of the file and parse it #
+    with open(ConfigFilePath, 'r') as File:
+        ConfigFileContents = File.read()
+
+    ConfigFileDictionary = yaml.safe_load(ConfigFileContents)
+
+    # Extract Values From Dictionary #
+    KafkaHost = str(ConfigFileDictionary.get('KafkaHost'))
+    KafkaPort = str(ConfigFileDictionary.get('KafkaPort'))
+
+    KafkaHost += f':{KafkaPort}'
+
+    return KafkaHost
