08a6b6633328dde4dd137671980cec5efe88ccff
datacrystals <tliao@carboncopies.org>
2021-05-18 00:23:42 +0000

    Updated Leader zNode Format From YAML To JSON

--- System/Core/Management/API/ManagementAPI.py
+++ System/Core/Management/API/ManagementAPI.py
@@ -13,10 +13,22 @@ Date-Created: 2021-05-17
 
 class ManagementAPISocketServer(): # Creates A Class To Connect To The Management API #
 
-    def __init__(self, MAPIConfig:dict, ZookeeperConfigDict:dict): # This function initialializes sockets #
+    def __init__(self, Logger, MAPIConfig:dict, ZookeeperConfigDict:dict): # This function initialializes sockets #
 
         # Get Config Params #
+        self.Logger = Logger
         self.Port = MAPIConfig['Port'] # Get the port from the port config
         self.IPAddr = ZookeeperConfigDict['ZKAddr'] # Get The IP Addr from the zoomeeper dict
 
+        # Create Socket Host Variable #
+        self.Logger.Log('Creating Host Variable')
+        self.SocketHost = (self.IPAddr, self.Port)
+
         # Bind To Port #
+        self.Logger.Log('Binding To Host')
+        self.Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.Socket.bind(self.SocketHost)
+
+        # Listen For Connections #
+        self.Logger.Log('Management API Backend Listening For Incoming Connections')
+        self.Socket.listen()
