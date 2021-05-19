8febfe0d6ca1936f64c75141df785a3a28cffe87
datacrystals <tliao@carboncopies.org>
2021-05-19 20:15:25 +0000

    Implement Socket MAPI

--- System/Core/Management/API/ManagementAPI.py
+++ System/Core/Management/API/ManagementAPI.py
@@ -3,6 +3,8 @@
 ###########################################################
 
 import socket
+import threading
+import json
 
 '''
 Name: Management API
@@ -18,7 +20,7 @@ class ManagementAPISocketServer(): # Creates A Class To Connect To The Managemen
         # Get Config Params #
         self.Logger = Logger
         self.Port = MAPIConfig['Port'] # Get the port from the port config
-        self.IPAddr = ZookeeperConfigDict['ZKAddr'] # Get The IP Addr from the zoomeeper dict
+        self.IPAddr = ZookeeperConfigDict['ZKHost'] # Get The IP Addr from the zoomeeper dict
 
         # Create Socket Host Variable #
         self.Logger.Log('Creating Host Variable')
@@ -32,3 +34,61 @@ class ManagementAPISocketServer(): # Creates A Class To Connect To The Managemen
         # Listen For Connections #
         self.Logger.Log('Management API Backend Listening For Incoming Connections')
         self.Socket.listen()
+
+        # Create MAPI Thread #
+        self.Logger.Log('Starting Management API Server Thread')
+        self.Thread = threading.Thread(target=self.ManagementAPIThread, args=())
+        self.Thread.start()
+
+
+    def ManagementAPIThread(self): # Create A Thread Function For The Management API #
+
+        # Wait Accept Incoming Connections #
+        self.Connection, self.ConnectionInformation = self.Socket.accept()
+        self.Logger.Log(f'Management API Recieved Connection From: {self.ConnectionInformation}')
+
+        # Enter Listening Loop To Recieve Commands #
+        while True:
+
+            # Get Command From Client #
+            self.Command = self.Connection.recv(65535)
+            self.Command = self.Command.decode()
+
+            # Convert To Dict From JSON #
+            self.Command = json.loads(self.Command)
+
+
+            # Check That Command Syntax Is Correct #
+            if str(type(self.Command)) != "<class 'dict'>":
+                self.Connection.send(json.dumps({"Response": "INVALID DICTIONARY FORMAT"}).encode())
+
+
+            if 'CallStack' not in self.Command:
+                self.Connection.send(json.dumps({"Response": "COMMAND DOES NOT INCLUDE 'CallStack' FIELD"}).encode())
+
+            # Check If Disconnect (Must be before SysName check to remain client agnostic) #
+            if self.Command['CallStack'] == 'Disconnect':
+                self.Connection.close()
+
+
+            if 'SysName' not in self.Command:
+                self.Connection.send(json.dumps({"Response": "COMMAND DOES NOT INCLUDE 'SysName' FIELD"}).encode())
+
+            if 'KeywordArgs' not in self.Command:
+                self.Connection.send(json.dumps({"Response": "COMMAND DOES NOT INCLUDE 'KeywordArgs' FIELD"}).encode())
+
+
+            if self.Command['SysName'] != 'NES':
+                self.Connection.send(json.dumps({"Response": "INVALID VALUE FOR 'SysName' FIELD"}).encode())
+
+            
+            # Built in Commands #
+            if self.Command['CallStack'] == 'TestAPI':
+                self.Connection.send(json.dumps({"Response": "but most of all, samy is my hero"}).encode())
+
+
+
+    def Quit(self): # Release The Socket #
+
+        # Close The Socket #
+        self.Socket.close()
