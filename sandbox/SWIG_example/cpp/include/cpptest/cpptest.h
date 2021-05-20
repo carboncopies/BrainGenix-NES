b2442bbf8fd1f91f432e57b217c3557b42c95e42
amukovozov <amukovozov@carboncopies.org>
2021-05-21 02:53:47 +0300

    Add a sandbox example of using SWIG to wrap C++ code for python usage.

---
+++ sandbox/SWIG_example/cpp/include/cpptest/cpptest.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <string>
+
+std::string Foo(const std::string& s);
+
---
+++ sandbox/SWIG_example/cpp/src/cpptest.cpp
@@ -0,0 +1,6 @@
+#include <cpptest/cpptest.h>
+
+std::string Foo(const std::string& s) {
+  return s + "-" + s;
+}
+
