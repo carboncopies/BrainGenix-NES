94ccd40c2c352101fb2c0665996bee62cc9b198a
J-French1 <85071286+J-French1@users.noreply.github.com>
2021-06-18 17:47:31 -0500

    Update DatabaseInterface.py

--- System/Core/Internode/Database/DatabaseInterface.py
+++ System/Core/Internode/Database/DatabaseInterface.py
@@ -6,7 +6,6 @@ import pymysql
 import atexit
 import re
 import threading
-import os
 
 '''
 Name: Database Interface
@@ -69,7 +68,7 @@ class DBInterface(): # Interface to MySQL database #
         self.DBConnection = pymysql.connect(host=Host, user=Username, password=Password, db=DatabaseName)
 
         # Start Pymysql Instance creator #
-        self.Thread = threading.Thread(target=Core.Internode.Database.DatabaseInterface.PymysqlInstanceCreate, args=(Logger))
+        self.Thread = threading.Thread(target=PymysqlInstanceCreate, args=(Logger))
         self.Thread.name = "PymysqlInstanceUpdate"
         self.Thread.start()
 
