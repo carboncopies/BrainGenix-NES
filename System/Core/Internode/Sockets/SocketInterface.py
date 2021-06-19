06533339ad4660eecf4888dada7e7ac80e60277c
datacrystals <tliao@carboncopies.org>
2021-06-19 16:53:31 +0000

    Identify Other Threads Not Using Thread Manager

--- System/Core/Internode/Sockets/SocketInterface.py
+++ System/Core/Internode/Sockets/SocketInterface.py
@@ -8,46 +8,46 @@ Description: This provides a level of abstraction between SocketIO operations an
 Date-Created: 2021-04-30
 '''
 
-import multiprocessing
-#import atexit
-#import socket
-import time
+# import multiprocessing
+# #import atexit
+# #import socket
+# import time
 
 
-class ProducerSocket(): # Class For Queue That Pushes Data To Another Server #
+# class ProducerSocket(): # Class For Queue That Pushes Data To Another Server #
 
-    def __init__(self, Address:str, Port:str, InputQueue:object): # Initialization #
+#     def __init__(self, Address:str, Port:str, InputQueue:object): # Initialization #
 
-        # Create Local Pointers #
-        self.Address = Address
-        self.Port = Port
+#         # Create Local Pointers #
+#         self.Address = Address
+#         self.Port = Port
 
-        # Create Socket "Server" (Consumer Connects To Producer) #
-        size = 0
-        t = time.time()
-        for z in range(100):
-            print(z)
-            size+=len(InputQueue.get(timeout=1))
+#         # Create Socket "Server" (Consumer Connects To Producer) #
+#         size = 0
+#         t = time.time()
+#         for z in range(100):
+#             print(z)
+#             size+=len(InputQueue.get(timeout=1))
 
-        print(size/(time.time()-t))
+#         print(size/(time.time()-t))
 
-D = b'asdfsadf'
-for z in range(25):
-    print(f'{z}/25')
-    D += D
-Q = multiprocessing.Queue()
-
-
-
-A = multiprocessing.Process(target=ProducerSocket, args=('', '', Q, ))
-A.start()
+# D = b'asdfsadf'
+# for z in range(25):
+#     print(f'{z}/25')
+#     D += D
+# Q = multiprocessing.Queue()
 
 
-print(len(D))
 
-for z in range(100):
-    print(f'{z}/100')
-    Q.put(D)
+# A = multiprocessing.Process(target=ProducerSocket, args=('', '', Q, ))
+# A.start()
+
+
+# print(len(D))
+
+# for z in range(100):
+#     print(f'{z}/100')
+#     Q.put(D)
 
 
 
