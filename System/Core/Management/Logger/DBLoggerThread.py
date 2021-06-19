74a6233b981886c52a5b24d76878ee2f285237ff
datacrystals <tliao@carboncopies.org>
2021-06-19 00:38:40 +0000

    Work on PyMySQL Error

--- System/Core/Management/Logger/DBLoggerThread.py
+++ System/Core/Management/Logger/DBLoggerThread.py
@@ -41,47 +41,22 @@ class DatabaseLogTransmissionSystem(): # Transmits Logs From The Logger To The D
         # Create Database Cursor #
         self.LoggerCursor = self.DatabaseConnection.cursor()
 
-#       # Create Thread #
-#       self.Logger.Log('Creating Database Log Transmission System Thread', 3)
-#       self.DatabaseLogTransmissionSystemThread = threading.Thread(target=self.__call__, args=(LogBufferQueue, DatabaseConfiguration,))
-#       self.DatabaseLogTransmissionSystemThread.name = 'DB Log Transmission'
-#       self.Logger.Log('Starting Thread for Database Log Transmission System', 3)
-#       self.DatabaseLogTransmissionSystemThread.start()
 
-
-    def ShouldStop():
+    def ShouldStop(self): # Check If Thread Should Exit #
         return not self.ControlQueue.empty()
 
 
-    def __call__(self, LogBufferQueue:object, DatabaseConfig:dict): # Thread To Transmit Logs To DB #
-
-#       # Connect To DB #
-#       DBUsername = str(DatabaseConfig.get('DatabaseUsername'))
-#       DBPassword = str(DatabaseConfig.get('DatabasePassword'))
-#       DBHost = str(DatabaseConfig.get('DatabaseHost'))
-#       DBDatabaseName = str(DatabaseConfig.get('DatabaseName'))
-#
-#
-#       # Connect To Database #
-#       self.DatabaseConnection = pymysql.connect(
-#           host = DBHost,
-#           user = DBUsername,
-#           password = DBPassword,
-#           db = DBDatabaseName
-#       )
-#
-#       # Create Database Cursor #
-#       self.LoggerCursor = self.DatabaseConnection.cursor()
-
+    def __call__(self): # Thread To Transmit Logs To DB #
 
         # Enter Loop #
         while not self.ShouldStop():
+
             # Read Log Item #
-            LogDataDict = LogBufferQueue.get()
+            LogDataDict = self.LogBufferQueue.get()
 
             # Decode Dict #
             Level = LogDataDict['LogLevel']
-            LogTime = LogDataDict['LogDatetime']
+            LogTime = LogDataDict['LogDateTime']
             CallingModuleName = LogDataDict['CallingModule']
             CallingFunctionName = LogDataDict['FunctionName']
             Message = LogDataDict['LogOutput']
@@ -93,3 +68,6 @@ class DatabaseLogTransmissionSystem(): # Transmits Logs From The Logger To The D
             val = (Level, str(LogTime), CallingModuleName.split("/")[-1].split(".")[0], CallingFunctionName, Message, Node)
             self.LoggerCursor.execute(insertStatement,val)
 
+
+    # Add function to close DB connection on thread exit
+    # self.DatabaseConnection.close()
