d0d3d8f75ba6f6820671cfd35b7e1bb5d0a98a86
datacrystals <tliao@carboncopies.org>
2021-06-24 20:44:53 +0000

    Update Versioning Information

--- System/Core/VersionData.py
+++ System/Core/VersionData.py
@@ -1,1 +1,12 @@
-VersionData = "0.0.7"
+###########################################################
+## This file is part of the BrainGenix Simulation System ##
+###########################################################
+
+'''
+Name: Version Data
+Description: This file is responsible for containing versioning information about the system
+Date-Created: 2021-06-24
+'''
+
+VersionNumber = '0.0.7'
+BranchVersion = 'dev'
