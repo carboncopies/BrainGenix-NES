2814956b0903c9d87a77e1510f04f0c902eabefa
J-French1 <85071286+J-French1@users.noreply.github.com>
2021-06-27 10:10:37 -0500

    Update ManagementAPI.py

--- System/Core/Management/API/ManagementAPI.py
+++ System/Core/Management/API/ManagementAPI.py
@@ -313,61 +313,61 @@ class ManagementAPISocketServer(): # Creates A Class To Connect To The Managemen
         # Sort Attributes #
         OutAttr = ['__']
 
-        # for Attr in Attributes:
-        #     Exists = False
-        #     Allow = False
-        #     Directory = True
-        #     Attr4 = []
-        #     Key = "LFTM"
-        #     if '__' not in str(Attr):
-        #         print(Attr)
-        #         while Directory:
-        #             mAPI = True
-        #             print("in")
-        #             if Allow == False:
-        #                 print("first")
-        #                 if "mAPI_" in str(Attr):
-        #                     mAPI = False
-        #                     print("1")
-        #                 Attr4 = Attr
-        #                 Allow = True
-        #                 Key1 = Key
-        #             if Allow:
-        #                 for Attr5 in Attr4:
-        #                     for Attr6 in dir(Key1 + "." + Attr5):
-        #                         if "mAPI_" in str(Attr6):
-        #                             mAPI = False
-        #                 print("2")
-        #             else:
-        #                 Directory = False
-        #             Dir = False
-        #             if Allow:
-        #                 Attr7 = Attr4
-        #                 Attr4 = []
-        #                 print("3")
-        #                 for Attr8 in Attr7:
-        #                     print("new")
-        #                     for Attr9 in dir(Key1 + "." + Attr8):
-        #                         print(Attr9)
-        #                         print("5")
-        #                         Attr4.append(Attr8 + "." + Attr9)
-        #                 print("end")
-        #             for Dir2 in (Attr4):
-        #                 if dir(Dir2) != []:
-        #                     Dir = True
-        #             if Dir != True or mAPI == False:
-        #                 Directory = False
+        for Attr in Attributes:
+            Exists = False
+            Allow = False
+            Directory = True
+            Attr4 = []
+            Key = "LFTM"
+            if '__' not in str(Attr):
+                print(Attr)
+                while Directory:
+                    mAPI = True
+                    print("in")
+                    if Allow == False:
+                        print("first")
+                        if "mAPI_" in str(Attr):
+                            mAPI = False
+                            print("1")
+                        Attr4 = Attr
+                        Allow = True
+                        Key1 = Key
+                    if Allow:
+                        for Attr5 in Attr4:
+                            for Attr6 in dir(Key1 + "." + Attr5):
+                                if "mAPI_" in str(Attr6):
+                                    mAPI = False
+                        print("2")
+                    else:
+                        Directory = False
+                    Dir = False
+                    if Allow:
+                        Attr7 = Attr4
+                        Attr4 = []
+                        print("3")
+                        for Attr8 in Attr7:
+                            print("new")
+                            for Attr9 in dir(Key1 + "." + Attr8):
+                                print(Attr9)
+                                print("5")
+                                Attr4.append(Attr8 + "." + Attr9)
+                        print("end")
+                    for Dir2 in (Attr4):
+                        if dir(Dir2) != []:
+                            Dir = True
+                    if Dir != True or mAPI == False:
+                        Directory = False
                 
-        #     print("4")
-        #     print(mAPI)
-        #     if mAPI == False:
-        #         for Object in OutAttr:
-        #             for Attr3 in Attributes:
-        #                 if Object == Attr3:
-        #                     Exists = True
-        #         if Exists == False:
-        #             OutAttr.append(Attr)
-        #             print("5")
+            print("4")
+            print(mAPI)
+            if mAPI == False:
+                for Object in OutAttr:
+                    for Attr3 in Attributes:
+                        if Object == Attr3:
+                            Exists = True
+                if Exists == False:
+                    OutAttr.append(Attr)
+                    print("5")
         del OutAttr[0]
 
         # Return Output #
