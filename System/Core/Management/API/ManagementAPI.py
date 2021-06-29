1c7a848f94b3b45168d6a2dd25768b766525de4d
datacrystals <tliao@carboncopies.org>
2021-06-29 15:15:46 -0700

    Implement Management API Help Command

--- System/Core/Management/API/ManagementAPI.py
+++ System/Core/Management/API/ManagementAPI.py
@@ -369,7 +369,43 @@ class ManagementAPISocketServer(): # Creates A Class To Connect To The Managemen
 
     def mAPI_Help(self, ArgumentsDictionary): # Provides Basic About The BGCLI #
 
+        # Set Default Help String #
         HelpMessage = 'This system provides a functional management interface to the BrainGenix system. Please use "ls Path=[path here]" to find commands, and use help Path=[path here] to find more information about a specific command. Please note that commands are seperated via a ".", so if calling command b nested under a, it would be "a.b".'
+
+        # Check For Path #
+        if 'Path' in ArgumentsDictionary:
+
+
+            # Extract Values From Command Dict #
+            CommandCallStack = ArgumentsDictionary['Path']
+
+
+            # Get Target Function #
+            Layers = CommandCallStack.split('.')
+            CommandFunction = self
+
+
+            # Iterate Through Layers, Run Command Called #
+            for LayerIndex in range(len(Layers)):
+
+                try:
+                    # Run Command With Prefix Included (mAPI_[Command Name]) #
+                    if LayerIndex < (len(Layers) - 1):
+                        CommandFunction = getattr(CommandFunction, Layers[LayerIndex])
+                    else:
+                        CommandFunction = getattr(CommandFunction, 'mAPI_'+Layers[LayerIndex])
+
+                    # Run Function #
+                    HelpMessage = CommandFunction.Help
+
+                except Exception as ErrorString:
+
+                    # Format Return Error For Return To Client #
+                    HelpMessage = 'Error: Command does not have a help message!'
+                    print(Exception)
+
+
+
 
         return HelpMessage
 
