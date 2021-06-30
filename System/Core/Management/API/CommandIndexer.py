fff42ff97a230501fbe59b211206c04e8d4aa1e0
datacrystals <tliao@carboncopies.org>
2021-06-30 00:55:11 -0700

    Create AttributesToDictionary Function

---
+++ System/Core/Management/API/CommandIndexer.py
@@ -0,0 +1,73 @@
+###########################################################
+## This file is part of the BrainGenix Simulation System ##
+###########################################################
+
+
+'''
+Name: Command Indexer
+Description: This file provides Management API Command Indexing Support.
+Date-Created: 2021-06-30
+'''
+
+
+from typing import Dict
+
+
+class Test1():
+
+    def __init__(self):
+        pass
+
+    def Attr1(self):
+        pass
+
+    def mAPI_Attr2(self):
+        pass
+
+
+class Test2():
+
+    def __init__(self):
+        self.InitTest1()
+
+    def Attr1(self):
+        pass
+
+    def mAPI_Attr2(self):
+        pass
+
+    def InitTest1(self):
+        self.Class1 = Test1()
+
+
+def AttributesToDictionary(DictionaryTarget, TargetPath, AttributeList): # Converts AttributeList To Dictionary Format #
+
+    # Get To Target Path #
+    TargetDictionaryLocation = DictionaryTarget
+    for KeyValue in TargetPath:
+        TargetDictionaryLocation = TargetDictionaryLocation[KeyValue]
+
+    # Convert Attribute List To Dict #
+    TempDict = {}
+    for Attribute in AttributeList:
+        TempDict.update({Attribute : {}})
+
+    # Update Original Dict #
+    TargetDictionaryLocation.update(TempDict)
+
+
+def IndexCommands(Target, RecursionDepth=5): # Creates Dictionary Tree Of All Commands With The Valid mAPI Prefix #
+
+    # Initialize Parameters #
+    OutputDictionary = {}
+
+
+    # Recursion Depth Loop #
+    for CurrentRecursionDepth in range(RecursionDepth - 1):
+
+        print(f'Current Depth Is: {CurrentRecursionDepth}')
+
+
+
+TestClass = Test2()
+IndexCommands(TestClass, RecursionDepth=5)
