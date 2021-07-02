f97addfa68ad33ad81a706e4bd0aa3c86b6c16d4
datacrystals <tliao@carboncopies.org>
2021-07-02 16:40:12 -0700

    Replace Platform Specific Config With SysConfig

--- System/Core/Initialization/Instantiator.py
+++ System/Core/Initialization/Instantiator.py
@@ -13,7 +13,7 @@ from Core.Management.Logger.Logger import SysLog
 
 
 
-def InstantiateLogger(DBConfig, SystemConfiguration): # Instantiates Kafka #
+def InstantiateLogger(SystemConfiguration): # Instantiates Kafka #
 
     # Log Message #
     print('Initializing Centralized Logging System')
@@ -22,7 +22,7 @@ def InstantiateLogger(DBConfig, SystemConfiguration): # Instantiates Kafka #
     # Instantiate Kafka #
     try:
 
-        Logger = SysLog(DBConfig, SystemConfiguration)
+        Logger = SysLog(SystemConfiguration, SystemConfiguration)
 
         # Log Success #
         Logger.Log('Centralized Logging Daemon Started')
