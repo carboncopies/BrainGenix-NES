4e7b2d122891c8141e9f9c601097e449663fb8ac
datacrystals <tliao@carboncopies.org>
2021-10-08 23:52:09 -0700

    Update Loader Function To Read Header Files

--- System/Core/Brian2Interface/CPPLoader.py
+++ System/Core/Brian2Interface/CPPLoader.py
@@ -2,6 +2,8 @@
 ## This file is part of the BrainGenix Simulation System ##
 ###########################################################
 
+import os
+
 '''
 Name: CPPLoader
 Description: This file is responsible for creating the C++ Code Loader For The Main Simulation Engine.
@@ -9,18 +11,42 @@ Date-Created: 2021-10-08
 '''
 
 # This Function Loads C++ Code As A String And Feeds It To The BrianInterface
-def LoadCPP(Logger:object, FileStreamObject:str):
+def LoadCPP(Logger:object, CPPRelativePath:str, HeaderRelativePath:str):
 
     # Log C++ Code Read
-    Logger.Log('Reading C++ Brian2Interface Callback Code', 3)
+    Logger.Log('Reading Brian2Interface C++ Callback Code', 3)
+
+
+    # Identify C++ Code Path
+    CurrentDirectory = os.path.abspath(os.path.dirname(__file__))
+    CPPCodeFilePath = os.path.join(CurrentDirectory, CPPRelativePath)
 
     # Read File
     with open(CPPCodeFilePath, 'r') as FileStreamObject:
 
-        FileString = FileStreamObject.read()
+        CPPFileString = FileStreamObject.read()
 
     # Log Completion
     Logger.Log('Successfully Read C++ Brian2Interface Callback Code', 2)
 
+
+
+
+    # Log Header Code Read
+    Logger.Log('Reading Brian2Interface Header Callback Code', 3)
+
+    # Identify Header Code Path
+    HeaderCodeFilePath = os.path.join(CurrentDirectory, HeaderRelativePath)
+
+    # Read File
+    with open(HeaderCodeFilePath, 'r') as FileStreamObject:
+
+        HeaderFileString = FileStreamObject.read()
+
+    # Log Completion
+    Logger.Log('Successfully Read Header Brian2Interface Callback Code', 2)
+
+
+
     # Return Filestream Object
-    return FileString
+    return CPPFileString, HeaderFileString
