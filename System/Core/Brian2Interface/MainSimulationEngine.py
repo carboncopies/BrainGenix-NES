08499c5cdd79aa3dd46c254f6b43e0f14258d6ad
datacrystals <tliao@carboncopies.org>
2021-10-08 23:38:56 -0700

    Create Main Simulation Engine File

---
+++ System/Core/Brian2Interface/MainSimulationEngine.py
@@ -0,0 +1,9 @@
+###########################################################
+## This file is part of the BrainGenix Simulation System ##
+###########################################################
+ 
+'''
+Name: Main Simulation Engine
+Description: This file is responsible for creating the Main Simulation Engine Class.
+Date-Created: 2021-10-08
+'''
