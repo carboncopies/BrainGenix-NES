1e3421607b15211c84ed1c6baaf8e7f588bf5958
datacrystals <tliao@carboncopies.org>
2021-10-08 23:37:11 -0700

    Write C++ Code Loading System

--- System/Core/Brian2Interface/CPPLoader.py
+++ System/Core/Brian2Interface/CPPLoader.py
@@ -3,4 +3,19 @@
 ###########################################################
 
 
-def LoadCPP
+# This Function Loads C++ Code As A String And Feeds It To The BrianInterface
+def LoadCPP(Logger:object, FileStreamObject:str):
+
+    # Log C++ Code Read
+    Logger.Log('Reading C++ Brian2Interface Callback Code', 3)
+
+    # Read File
+    with open(CPPCodeFilePath, 'r') as FileStreamObject:
+
+        FileString = FileStreamObject.read()
+
+    # Log Completion
+    Logger.Log('Successfully Read C++ Brian2Interface Callback Code', 2)
+
+    # Return Filestream Object
+    return FileString
