ec348211a1e8eea719e41474f3539ef3a3150dba
datacrystals <tliao@carboncopies.org>
2021-10-08 23:58:17 -0700

    Move BrianInterface To CPPSource Directory

---
+++ System/Core/Brian2Interface/CPPSource/BrianInterface.cpp
@@ -0,0 +1,8 @@
+//------------------------------------------------------------//
+// This file is part of the BrainGenix::NES Simulation System //
+//------------------------------------------------------------//
+
+/*
+    NOTE: THIS FILE IS LOADED AT RUNTIME AND ADDED AS A CALLBACK TO THE BRIAN2 SYSTEM
+*/
+
