353a262aa42519373bb87180c5a51fb55197bcb5
datacrystals <tliao@carboncopies.org>
2021-10-10 17:05:05 -0700

    Move Brian Code To Brian2Interface Subfolder

--- System/Core/MainSimulationEngine/CPPSource/BrianInterface.h
+++
@@ -1,8 +0,0 @@
-//------------------------------------------------------------//
-// This file is part of the BrainGenix::NES Simulation System //
-//------------------------------------------------------------//
-
-/*
-    NOTE: THIS FILE IS LOADED AT RUNTIME AND ADDED AS A CALLBACK TO THE BRIAN2 SYSTEM
-*/
-
