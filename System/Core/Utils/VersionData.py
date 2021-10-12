f65c076a95cee075a9b706d559301972bcec75c9
datacrystals <tliao@carboncopies.org>
2021-10-12 16:12:37 -0700

    Move VersionData To Utils

---
+++ System/Core/Utils/VersionData.py
@@ -0,0 +1,12 @@
+###########################################################
+## This file is part of the BrainGenix Simulation System ##
+###########################################################
+
+'''
+Name: Version Data
+Description: This file is responsible for containing versioning information about the system
+Date-Created: 2021-06-24
+'''
+
+VersionNumber = '0.0.7'
+BranchVersion = 'dev'
