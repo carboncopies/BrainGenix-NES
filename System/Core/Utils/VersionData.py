377367552040cdfbb4f437035fcf78fdb82174dc
Thomas Liao <tliao@carboncopies.org>
2021-11-11 01:20:59 -0800

    Update NES Version Metadata

--- System/Core/Utils/VersionData.py
+++ System/Core/Utils/VersionData.py
@@ -8,5 +8,5 @@ Description: This file is responsible for containing versioning information abou
 Date-Created: 2021-06-24
 '''
 
-VersionNumber = '0.0.7'
+VersionNumber = '0.0.8'
 BranchVersion = 'dev'
