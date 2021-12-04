b37d23f5b24fa372cbf370a3562bcfe94dcdc027
datacrystals <tliao@carboncopies.org>
2021-12-03 16:10:09 -0800

    Create Empty Model

---
+++ MSE/Source/Core/Models/NES_EmptyModel/NES_EmptyModel.cpp
@@ -0,0 +1,14 @@
+//=================================================================//
+// This file is part of the BrainGenix-NES Neuron Emulation System //
+//=================================================================//
+
+/*
+    Description: This file is responsible for implementing a template model for testing.
+    Documentation Status: Complete
+    Additonal Notes: None
+    Date Created: 2021-12-03
+*/ 
+
+#include <NES_EmptyModel.h>
+
+
