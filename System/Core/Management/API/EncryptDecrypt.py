bffdfd51a95b2bd1ee304ff06812b22c5109d5a9
Prishita Ray <prishita.r85@gmail.com>
2021-12-18 16:39:38 +0530

    Create EncryptDecrypt.py

---
+++ System/Core/Management/API/EncryptDecrypt.py
@@ -0,0 +1,28 @@
+import pycrypto
+import random
+
+AESkey = ''.join(chr(random.randint(0, 0xFF)) for i in range(16))
+iv = ''.join([chr(random.randint(0, 0xFF)) for i in range(16)])
+aes = AES.new(AESkey, AES.MODE_CBC, iv)
+
+def EncryptCommand(OriginalCommand: dict):
+  
+   EncryptedCommand={}
+  
+   for key, value in OriginalCommand.items():
+    
+      key= aes.encrypt(key)
+      EncryptedCommand[key] = aes.encrypt(value)
+   
+   return EncryptedCommand
+
+def DecryptCommand(EncryptedCommand: dict):
+  
+   DecryptedCommand={}
+  
+   for key, value in EncryptedCommand.items():
+    
+      key= aes.decrypt(key)
+      DecryptedCommand[key] = aes.decrypt(value)
+   
+   return DecryptedCommand
