13457f43b4de7a80a68aef6aa3befef087388d32
Prishita Ray <prishita.r85@gmail.com>
2021-12-26 17:32:37 +0530

    Resolve AESkey and IV should be 16 bytes error

--- System/Core/Management/API/EncryptDecrypt.py
+++ System/Core/Management/API/EncryptDecrypt.py
@@ -1,28 +1,20 @@
 from Crypto.Cipher import AES
-import random
+import os
 
-AESkey = ''.join(chr(random.randint(0, 0xFF)) for i in range(16))
-iv = ''.join([chr(random.randint(0, 0xFF)) for i in range(16)])
+AESkey = os.urandom(16)
+iv = os.urandom(16)
 aes = AES.new(AESkey, AES.MODE_CBC, iv)
 
-def EncryptCommand(OriginalCommand: dict):
+
+def EncryptCommand(OriginalCommand: str):
   
-   EncryptedCommand={}
-  
-   for key, value in OriginalCommand.items():
-    
-      key= aes.encrypt(key)
-      EncryptedCommand[key] = aes.encrypt(value)
+   EncryptedCommand= aes.encrypt(OriginalCommand)
    
    return EncryptedCommand
+  
 
-def DecryptCommand(EncryptedCommand: dict):
+def DecryptCommand(EncryptedCommand: str):
   
-   DecryptedCommand={}
-  
-   for key, value in EncryptedCommand.items():
-    
-      key= aes.decrypt(key)
-      DecryptedCommand[key] = aes.decrypt(value)
+   DecryptedCommand= aes.decrypt(EncryptedCommand)
    
    return DecryptedCommand
