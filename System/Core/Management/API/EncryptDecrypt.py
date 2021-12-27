8e3afdfcc5ee843a11e2fc08a592f0599dfc4d8f
Prishita Ray <prishita.r85@gmail.com>
2021-12-27 19:38:50 +0530

    Update EncryptDecrypt.py

--- System/Core/Management/API/EncryptDecrypt.py
+++ System/Core/Management/API/EncryptDecrypt.py
@@ -1,20 +1,22 @@
 from Crypto.Cipher import AES
-import os
+import random, string, base64
 
-AESkey = os.urandom(16)
-iv = os.urandom(16)
-aes = AES.new(AESkey, AES.MODE_CBC, iv)
-
+AESkey = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for x in range(32))
+iv = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for x in range(16))
 
 def EncryptCommand(OriginalCommand: str):
-  
-   EncryptedCommand= aes.encrypt(OriginalCommand)
-   
+
+   aes = AES.new(AESkey, AES.MODE_CFB, iv)
+   EncryptedCommand = aes.encrypt(OriginalCommand)
+   EncryptedCommand = base64.b64encode(EncryptedCommand)
+
    return EncryptedCommand
-  
 
 def DecryptCommand(EncryptedCommand: str):
-  
-   DecryptedCommand= aes.decrypt(EncryptedCommand)
-   
+
+   EncryptedCommand = base64.b64decode(EncryptedCommand)
+   aes = AES.new(AESkey, AES.MODE_CFB, iv)
+   DecryptedCommand = aes.decrypt(EncryptedCommand)
+   # DecryptedCommand = DecryptedCommand[0:len(DecryptedCommand)//16]
+
    return DecryptedCommand
