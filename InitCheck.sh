e702bca833e4f4d3a6fee5ae648d6b8696638e76
Prishita Ray <prishita.r85@gmail.com>
2022-02-13 03:12:55 -0500

    Update InitCheck.sh

--- InitCheck.sh
+++ InitCheck.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+sudo apt-get install yamllint
+pip install shyaml
+
 files= find . -type f -name '*.yaml'
 
 for file in files
 do
    echo file
    source file
+   
+   # Check validity of config file
+   yamllint file
+   
+   # Return data values after parsing config file
+   cat test.yaml | shyaml values-0 | \
+   while read -r -d $'\0' value; do
+       echo "RECEIVED: '$value'"
+   done
+   
 done
