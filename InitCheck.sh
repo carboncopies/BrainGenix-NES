83462ce1679a135ff38439a8f7da3eee43a62d43
Prishita Ray <prishita.r85@gmail.com>
2022-03-06 03:11:08 -0500

    Regex to extract IPs

--- InitCheck.sh
+++ InitCheck.sh
@@ -2,24 +2,8 @@
 
 enable -n local
 
-parse_check () {
-   local prefix=$2
-   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
-   sed -ne "s|^\($s\):|\1|" \
-        -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
-        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
-   awk -F$fs '{
-      indent = length($1)/2;
-      vname[indent] = $2;
-      for (i in vname) {if (i > indent) {delete vname[i]}}
-      if (length($3) > 0) {
-          printf("%s\n",$3)
-      }
-   }'
-}
-
 for file in **/*.yaml;
 do
    echo $file
-   parse_check $file
+   sed -n '/^.*\..*\..*\..*$/p' $file
 done
