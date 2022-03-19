a95a26ab0f01a4dd17fabdf5d929759442946080
datacrystals <tliao@carboncopies.org>
2022-03-19 18:51:57 +0000

    Refactor NES Build System

--- MSE/CMake/Utils/DisableTargetWarnings.cmake
+++ MSE/CMake/Utils/DisableTargetWarnings.cmake
@@ -3,12 +3,12 @@
 ##################
 
 function(disable_target_warnings target)
-    NES.BuildLogger(${Green} "Disabling Warnings For Target ${target}")
+    NESBuildLogger(${Green} "Disabling Warnings For Target ${target}")
     target_compile_options(${target} PRIVATE
         $<$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>,$<CXX_COMPILER_ID:GNU>>:
             -w>
         $<$<CXX_COMPILER_ID:MSVC>:
         /w>
         )
-    NES.BuildLogger(${BoldGreen} "Disabled Warnings For Target ${target}")
+    NESBuildLogger(${BoldGreen} "Disabled Warnings For Target ${target}")
 endfunction()
