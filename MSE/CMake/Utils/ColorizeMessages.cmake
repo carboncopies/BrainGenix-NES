a15f8d9436a53731284ccfb46ffeb5ab6f3eaa29
datacrystals <tliao@carboncopies.org>
2022-03-19 18:51:30 +0000

    Refacotor NES Build System

--- MSE/CMake/Utils/ColorizeMessages.cmake
+++ MSE/CMake/Utils/ColorizeMessages.cmake
@@ -41,8 +41,8 @@ else ()
     set(BoldWhite   " ")
 endif()
 
-function(ERSBuildLogger Color Message)
-    message("${BoldMagenta}[ERS Build System]${ColourReset} ${Color}${Message}${ColourReset}")
+function(NESBuildLogger Color Message)
+    message("${BoldMagenta}[NES Build System]${ColourReset} ${Color}${Message}${ColourReset}")
 endfunction()
 
 # function(message)
