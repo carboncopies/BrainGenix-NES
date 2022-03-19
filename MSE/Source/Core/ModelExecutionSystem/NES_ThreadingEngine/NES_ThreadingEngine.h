1f35e78e13aa9b91fa45e8b3f5cdd19e6a311bf4
datacrystals <tliao@carboncopies.org>
2022-03-19 18:34:55 +0000

    Cleanup NES Threading Engine Header

--- MSE/Source/Core/ModelExecutionSystem/NES_ThreadingEngine/NES_ThreadingEngine.h
+++ MSE/Source/Core/ModelExecutionSystem/NES_ThreadingEngine/NES_ThreadingEngine.h
@@ -27,49 +27,48 @@
 
 class NES_CLASS_ThreadingEngine {
 
-    private:
+private:
 
-        // Neuron Vector(s) will need to add more vectors here for other neuron types in the future
-        std::vector<std::vector<EmptyNeuronModel>> NeuronModelVector_EmptyNeuronModel; /**<Vector Contaiining Empty Neuron Models*/
-        std::mutex ThreadBlockMutexes_[65535]; /*<<Thread Block Mutex Array*/
-        std::mutex ThreadExitMutexes_[65535]; /*<<Vector To Exit Threads*/
-
-
-        // Member Vars
-        NES_CLASS_LoggerClass *Logger_; /**<Logger Instance Pointer*/
-        std::vector<std::thread> ThreadList_; /**<Vector containing worker threads.*/
-
-        int CPUCount_; /**<Number of cpu cores in system*/
+    // Neuron Vector(s) will need to add more vectors here for other neuron types in the future
+    std::vector<std::vector<EmptyNeuronModel>> NeuronModelVector_EmptyNeuronModel; /**<Vector Contaiining Empty Neuron Models*/
+    std::mutex ThreadBlockMutexes_[65535]; /*<<Thread Block Mutex Array*/
+    std::mutex ThreadExitMutexes_[65535]; /*<<Vector To Exit Threads*/
 
 
+    // Member Vars
+    NES_CLASS_LoggerClass *Logger_; /**<Logger Instance Pointer*/
+    std::vector<std::thread> ThreadList_; /**<Vector containing worker threads.*/
 
-    private:
-        void WorkerThread(std::vector<EmptyNeuronModel>* Neurons, std::mutex* BlockThread, std::mutex* ExitThread);
-
-
-    public:
-
-        /**
-         * @brief Construct a new nes class threadingengine object
-         * 
-         */
-        NES_CLASS_ThreadingEngine(NES_CLASS_LoggerClass *Logger);
-
-
-        /**
-         * @brief Destroy the nes class threadingengine object
-         * 
-         */
-        ~NES_CLASS_ThreadingEngine();
-
-        /**
-         * @brief Create the given number of neurons and add them to the neurons vector. NOTE: THE NUMBER OF NEURONS MUST BE A MULTIPLE OF THE NUMBER OF THREADS!
-         * @note This is temporary, and will need to be updated later to add multiscale support!
-         */
-        void CreateNeurons(int NumberNeurons);
+    int CPUCount_; /**<Number of cpu cores in system*/
 
 
 
-        //void Test(const char* Msg, int i, int *counter);
+private:
+    void WorkerThread(std::vector<EmptyNeuronModel>* Neurons, std::mutex* BlockThread, std::mutex* ExitThread);
+
+
+public:
+
+    /**
+     * @brief Construct a new nes class threadingengine object
+     * 
+     */
+    NES_CLASS_ThreadingEngine(NES_CLASS_LoggerClass *Logger);
+
+
+    /**
+     * @brief Destroy the nes class threadingengine object
+     * 
+     */
+    ~NES_CLASS_ThreadingEngine();
+
+    /**
+     * @brief Create the given number of neurons and add them to the neurons vector. NOTE: THE NUMBER OF NEURONS MUST BE A MULTIPLE OF THE NUMBER OF THREADS!
+     * @note This is temporary, and will need to be updated later to add multiscale support!
+     */
+    void CreateNeurons(int NumberNeurons);
+
+
+
 
 };
