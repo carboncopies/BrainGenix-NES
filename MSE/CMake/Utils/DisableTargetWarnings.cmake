a372bc46b33878ef9637099a6ba28787b45164cd
datacrystals <tliao@carboncopies.org>
2022-03-19 18:51:41 +0000

    Refacotor NES Build System

--- MSE/CMake/Utils/DisableTargetWarnings.cmake
+++ MSE/CMake/Utils/DisableTargetWarnings.cmake
@@ -1,14 +1,14 @@
 ##################
-# ERS BUILD UTIL #
+# NES BUILD UTIL #
 ##################
 
 function(disable_target_warnings target)
-    ERSBuildLogger(${Green} "Disabling Warnings For Target ${target}")
+    NES.BuildLogger(${Green} "Disabling Warnings For Target ${target}")
     target_compile_options(${target} PRIVATE
         $<$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>,$<CXX_COMPILER_ID:GNU>>:
             -w>
         $<$<CXX_COMPILER_ID:MSVC>:
         /w>
         )
-    ERSBuildLogger(${BoldGreen} "Disabled Warnings For Target ${target}")
+    NES.BuildLogger(${BoldGreen} "Disabled Warnings For Target ${target}")
 endfunction()
