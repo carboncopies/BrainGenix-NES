3ad0c3af0c4943c2d6d5c8588212c06132f13807
datacrystals <tliao@carboncopies.org>
2022-03-19 18:45:43 +0000

    Update NES Logging System

---
+++ MSE/Source/Core/Structures/ERS_STRUCT_RGBColor/ERS_STRUCT_RGBColor.h
@@ -0,0 +1,26 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+#pragma once
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <memory.h>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+
+// Internal Libraries (BG convention: use <> instead of "")
+
+
+/**
+ * @brief This class stores an rgb color value.
+ * 
+ */
+struct ERS_STRUCT_RGBColor {
+
+    int Red; /**<Red Component*/
+    int Green; /**<Green Component*/
+    int Blue; /**<Blue Component*/
+
+
+};
