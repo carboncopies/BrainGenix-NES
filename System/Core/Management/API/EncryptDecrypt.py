1a4ae680ab2141f42ff44ad02463bab508ad2238
Prishita Ray <prishita.r85@gmail.com>
2022-04-10 01:53:29 -0400

    Doxygen Formatting - EncryptDecrypt

--- System/Core/Management/API/EncryptDecrypt.py
+++ System/Core/Management/API/EncryptDecrypt.py
@@ -1,9 +1,14 @@
+## @package EncryptDecrypt
+# Encrypts and Decrypts commands between the UI and NES
+
 from Crypto.Cipher import AES
 import random, string, base64
 
 AESkey = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for x in range(32))
 iv = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for x in range(16))
 
+## Encrypts commands issued by the user through UI to NES
+# @param OriginalCommand Command issued in UI
 def EncryptCommand(OriginalCommand: str):
 
    aes = AES.new(AESkey, AES.MODE_CFB, iv)
@@ -12,6 +17,8 @@ def EncryptCommand(OriginalCommand: str):
 
    return EncryptedCommand
 
+## Decrypts commands in NES for actual execution
+# @param EncryptedCommand Encrypted Command present in NES
 def DecryptCommand(EncryptedCommand: str):
 
    EncryptedCommand = base64.b64decode(EncryptedCommand)
