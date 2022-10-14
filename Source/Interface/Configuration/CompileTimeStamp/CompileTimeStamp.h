8321e2598f37251db8b98e9c95e8c7417bdf2970
datacrystals <tliao@carboncopies.org>
2022-10-14 01:47:11 +0000

    Fix Naming Convention

---
+++ Source/Interface/Configuration/CompileTimeStamp/CompileTimeStamp.h
@@ -0,0 +1,8 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+#pragma once
+
+
+#define ERS_COMPILE_TIME_STAMP "2022-10-14 01h10:1665711925 (UTC)"  // Timestamp when build was made
