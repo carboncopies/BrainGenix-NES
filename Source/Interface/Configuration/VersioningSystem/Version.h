7eee90ee2cc1ceda549c27ab868165b881acf5a9
datacrystals <tliao@carboncopies.org>
2022-10-14 01:48:07 +0000

    Fix Naming Convention

---
+++ Source/Interface/Configuration/VersioningSystem/Version.h
@@ -0,0 +1,7 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+#pragma once
+
+#define ERS_VERSION "0.4.0"
