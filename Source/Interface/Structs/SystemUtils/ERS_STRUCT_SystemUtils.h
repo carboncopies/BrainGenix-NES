9e017a9e898b81601f3372a0eee8d993c05718bf
datacrystals <tliao@carboncopies.org>
2022-10-14 01:39:21 +0000

    Fix Compiler Errors

--- Source/Interface/Structs/SystemUtils/ERS_STRUCT_SystemUtils.h
+++ Source/Interface/Structs/SystemUtils/ERS_STRUCT_SystemUtils.h
@@ -13,14 +13,6 @@
 // Internal Libraries (BG convention: use <> instead of "")
 #include <ERS_LoggingSystem.h>
 #include <ERS_HardwareInformation.h>
-#include <ERS_CLASS_PythonInterpreterIntegration.h>
-
-#include <ERS_STRUCT_RendererSettings.h>
-
-#include <ERS_InputOutputSubsystem.h>
-#include <ERS_FramerateManager.h>
-
-
 
 
 
@@ -38,19 +30,6 @@ struct ERS_STRUCT_SystemUtils {
     std::unique_ptr<bool> SystemShouldRun_; /**<Pointer To Variable Setting If System Should Run Or Not*/
 
     std::unique_ptr<ERS_LoggingSystem> Logger_; /**<Pointer To BG-ERS Logging System*/
-    std::unique_ptr<ERS_InputOutputSubsystem> ERS_IOSubsystem_; /**<Pointer To ERS Input Output Subsystem*/
-    std::unique_ptr<ERS_CLASS_FramerateManager> FramerateManager_; /**<Pointer to framerate subsystem*/
-    std::unique_ptr<ERS_HardwareInformation> ERS_HardwareInformation_; /**<Pointer to the hardware informaton class instance*/
-    std::unique_ptr<ERS_CLASS_PythonInterpreterIntegration> ERS_CLASS_PythonInterpreterIntegration_; /**<Pointer to the class integrating the python interpreter*/
-
-    std::unique_ptr<ERS_STRUCT_RendererSettings> RendererSettings_; /**<Contains settings to be used by the renderer class, Automatically updates the renderer when this struct is updated.*/
-
-
-    int RenderWidth_ = 0; /**<Width of the display being rendered to (if using a window, this is the size of the window)*/
-    int RenderHeight_ = 0; /**<Height of the display being rendered to (if using a window, this is the size of the window)*/
-    
-
-    bool IsLinux_ = true; /**<Boolean Value Set To True When Binary Is Compiled For Linux, False Otherwise*/
 
 
 };
