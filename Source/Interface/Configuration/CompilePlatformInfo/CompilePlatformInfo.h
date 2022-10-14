96e5cfb77af9947666864a09b17c54aaa7c6fa01
datacrystals <tliao@carboncopies.org>
2022-10-14 01:44:23 +0000

    Fix Naming Convention

---
+++ Source/Interface/Configuration/CompilePlatformInfo/CompilePlatformInfo.h
@@ -0,0 +1,14 @@
+//======================================================================//
+// This file is part of the BrainGenix-ERS Environment Rendering System //
+//======================================================================//
+
+#pragma once
+
+
+#define ERS_TARGET_OS_NAME    "Linux"    // Name of the operating system which ERS is being built for.
+#define ERS_TARGET_OS_VERSION "5.15.0-50-generic" // Version of the operating system which ERS is being built for.
+#define ERS_TARGET_PROCESSOR  "x86_64"  // Name of the CPU which ERS is being built for.
+
+#define ERS_HOST_OS_NAME     "Linux"    // Name of the operating system which is compiling ERS.
+#define ERS_HOST_OS_VERSION  "5.15.0-50-generic" // Version of the operating system which is compiling ERS.
+#define ERS_HOST_PROCESSOR   "x86_64"  // Name of the CPU which is compiling ERS.
