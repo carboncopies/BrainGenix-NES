90e127683f84d3757a734dad0df44de1c1fa1790
datacrystals <tliao@carboncopies.org>
2023-04-07 22:14:26 +0000

    Create Sandbox For CTests

---
+++ Sandbox/CTests/Source/Submodule/AddTwoNumbers.cpp
@@ -0,0 +1,4 @@
+// sample function that can be used for ctests
+int AddTwoNums(int a, int b) {
+    return a + b;
+}
---
+++ Sandbox/CTests/Source/Submodule/AddTwoNumbers.h
@@ -0,0 +1,2 @@
+// sample function that can be used for ctests
+int AddTwoNums(int a, int b);
