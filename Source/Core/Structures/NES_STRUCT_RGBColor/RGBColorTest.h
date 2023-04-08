34e4e4676775a4a907f90e4259c206199e2ea037
Thomas Liao <tliao@carboncopies.org>
2023-04-08 05:14:20 +0100

    Add CTests

---
+++ Source/Core/Structures/NES_STRUCT_RGBColor/RGBColorTest.h
@@ -0,0 +1,14 @@
+//=================================================================//
+// This file is part of the BrainGenix-NES Neuron Emulation System //
+// Unit Test File                                                  //
+//=================================================================//
+
+#pragma once
+
+// Standard Libraries (BG convention: use <> instead of "")
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+
+// Internal Libraries (BG convention: use <> instead of "")
+
+
