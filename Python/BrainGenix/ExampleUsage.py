17f4d7da25adf3d991f982137c15765a6f15e7ae
Thomas <tliao@carboncopies.org>
2023-05-22 13:11:51 -0700

    Add Diagram

--- Python/BrainGenix/ExampleUsage.py
+++ Python/BrainGenix/ExampleUsage.py
@@ -1,13 +1,24 @@
 # Test Driver
 
+import random
 
 import NES.Client
+import NES.Models
+import NES.Simulation
+
+
+def RandXYZ():
+    X = random.random()*50
+    Y = random.random()*50
+    Z = random.random()*50
+    return (X,Y,Z)
+
 
 def main():
 
     print("Starting NES Frontend Tests")
 
-    # Setup Client
+    # Setup Client (use local config)
     ClientConfiguration = NES.Client.Configuration.Configuration()
     ClientConfiguration.Mode = NES.Client.Modes.Local
 
@@ -15,6 +26,43 @@ def main():
 
     if not ClientInstance.IsConnected():
         print("Cannot Connect To Client")
+
+
+    # Create Simulation
+    MySim = NES.Simulation.Simulation()
+    MySim.Name = "Test Sim"
+    
+    SimID = ClientInstance.CreateSimulation(MySim)
+    if (SimID == -1):
+        print("Failed To Create Simulation!")
+        exit()
+    ClientInstance.BindSimulation(SimID)
+
+    
+    # Now Add Neurons
+    NumNeurons = 100
+    
+    NeuronIDs = []
+    for i in range(NumNeurons):
+
+        # We represent neurons as follows:
+        #               0 0  <- Synapese+Receptors (too small to show)              
+        #               | |                
+        #               ===  <- Compartments               
+        #                |                 
+        #              +---+                   
+        #              |   | <- Soma                 
+        #              +---+                   
+        #                |                 
+        #         ============== <- More Compartments          
+        #         0     |  0   |              
+        #               0      0  <- More Synapses+Receptors          
+
+        NeuronConfig = NES.Models.Neurons.TestNeuron()
+        NeuronConfig.Position = RandXYZ() 
+
+        NeuronConfig.Axons = []
+
 
 
 if __name__ == "__main__":
