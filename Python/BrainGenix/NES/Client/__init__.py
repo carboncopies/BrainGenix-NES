1ced9a3e1d8a64677ed81035d669b6a4aba1af86
Thomas Liao <tliao@carboncopies.org>
2023-05-22 23:22:27 -0700

    Update Project Structure

--- Python/BrainGenix/NES/Client/__init__.py
+++ Python/BrainGenix/NES/Client/__init__.py
@@ -1,3 +1,10 @@
 from . import Client
 from . import Configuration
 from . import Modes
+
+__all__ = [
+
+    "Client",
+    "Configuration",
+    "Modes"
+]
--- Python/Tools/Clean.sh
+++ Python/Tools/Clean.sh
@@ -2,3 +2,5 @@
 
 cd ..
 rm -rf dist
+rm -rf TestEnv
+rm -rf *.egg-info
