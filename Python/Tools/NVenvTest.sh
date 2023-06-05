4f7bac092b189181980672c333216649e8d3a51c
Thomas Liao <tliao@carboncopies.org>
2023-06-05 13:03:52 -0700

    Update Test Script

---
+++ Python/Tools/NVenvTest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Setup And Enter Test Directory
+cd ..
+mkdir -p .Test
+cp -r BrainGenix .Test
+cp -r Tools/* .Test/BrainGenix
+cd .Test/BrainGenix
+
+# Run Example Usage Code
+echo "Running Simple API Test"
+python3 SimpleTest.py
+echo "Running More Extensive API Test"
+python3 ExampleUsage.py
