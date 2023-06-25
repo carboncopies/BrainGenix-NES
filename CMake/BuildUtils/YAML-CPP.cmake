d5a03ee53ef64e3c790b4677eaf67d5937ea84de
Thomas Liao <tliao@carboncopies.org>
2023-06-24 21:34:17 -0700

    Clean Repo

--- CMake/BuildUtils/YAML-CPP.cmake
+++
@@ -1,8 +0,0 @@
-##########################
-# Builds Library For NES #
-##########################
-
-NESBuildLogger(${Green} "Configuring YAML-CPP Library")
-add_subdirectory("${LIB_DIR}/yaml-cpp")
-disable_target_warnings(yaml-cpp)
-NESBuildLogger(${BoldGreen} "Finished Configuring YAML-CPP Library")
