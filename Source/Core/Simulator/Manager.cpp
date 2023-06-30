d7c17acb482b936be3a82425fe200098f95fa05d
Thomas Liao <tliao@carboncopies.org>
2023-06-29 21:46:10 -0700

    Remove Extra Code

--- Source/Core/Simulator/Manager.cpp
+++ Source/Core/Simulator/Manager.cpp
@@ -46,38 +46,6 @@ Manager::~Manager() {
 }
 
 
-// void Manager::SimulationEngineThread(Simulation* _Sim) {
-
-//     // Log Init message
-//     std::cout<<"[Info] Starting Simulation Updater Thread\n";
-
-//     // Enter into loop until thread should stop
-//     while (!StopThreads_) {
-
-//         if (_Sim->WorkRequested) {
-//             _Sim->IsProcessing = true;
-
-//             if (_Sim->CurrentTask == SIMULATION_RESET) {
-//                 std::cout<<"[Info] Worker Performing Simulation Reset For Simulation "<<_Sim->ID<<std::endl;
-//                 _Sim->CurrentTask = SIMULATION_NONE;
-//                 _Sim->WorkRequested = false;
-
-//             } else if (_Sim->CurrentTask == SIMULATION_RUNFOR) {
-//                 std::cout<<"[Info] Worker Performing Simulation RunFor For Simulation "<<_Sim->ID<<std::endl;
-//                 _Sim->CurrentTask = SIMULATION_NONE;
-//                 _Sim->WorkRequested = false;
-//             }
-
-//             _Sim->IsProcessing = false;
-//         } else {
-//             std::this_thread::sleep_for(std::chrono::milliseconds(10)); // sleep for 10ms
-//         }
-
-//     }
-
-// }
-
-
 std::string Manager::SimulationCreate(std::string _JSONRequest) {
 
     // Parse Request
