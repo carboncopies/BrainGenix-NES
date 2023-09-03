6bf333f6e426d96a5ab4dcceb4853b710e5afe84
Debajyoti Debnath <ddebnath@carboncopies.org>
2023-09-03 19:33:02 -0400

    Modify Setup.sh to work for fedora and arch linux
    Still need to fix dependency resolution in target linux distribution. E.g. g++ is not available in pacman for arch.

--- Tools/Setup.sh
+++ Tools/Setup.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 
 # Detect if running as root
-APT_COMMAND_PREFIX=""
+SUDO_PREFIX=""
 echo "Detecting If Script Running As root"
 if [ "$EUID" -ne 0 ]; then
     echo "Detected Install Script Is Not Running As Root, Adding 'sudo' Prefix To Commands"
-    APT_COMMAND_PREFIX="sudo"
+    SUDO_PREFIX="sudo"
 else
     echo "Command Running As Root, Running Commands Without 'sudo'"
 fi
 
+# Setting up package manager depending on Linux Distro
+DISTRO=$(cat /etc/os-release | grep -w ID | awk -F'=' '{print $2}')
+PACKAGE_MANAGER_INSTALL_CMD=""
+PACKAGE_MANAGER_UPDATE_CMD=""
+
+case $DISTRO in
+    "arch") PACKAGE_MANAGER_INSTALL_CMD="pacman -S";
+            PACKAGE_MANAGER_UPDATE_CMD="pacman -Syu";;
+    "ubuntu") PACKAGE_MANAGER_INSTALL_CMD="apt install -y";
+              PACKAGE_MANAGER_UPDATE_CMD="apt update";;
+    "fedora") PACKAGE_MANAGER_INSTALL_CMD="dnf install -y";
+              PACKAGE_MANAGER_UPDATE_CMD="dnf update";;
+    *) PACKAGE_MANAGER_INSTALL_CMD="apt install";
+       PACKAGE_MANAGER_UPDATE_CMD="dnf update";;
+esac
 
 # Setup, update package registry
 echo "Setting Up Packages Needed For Compilation"
-$APT_COMMAND_PREFIX apt update || exit 1
-
+$SUDO_PREFIX $PACKAGE_MANAGER_UPDATE_CMD || exit 1
 
 
 # Install Compiler Tools
@@ -24,9 +38,10 @@ COMPILER_DEPS="git wget cmake g++"
 VCPKG_DEPS="curl zip unzip tar pkg-config autoconf flex bison"
 
 # Install Everything
-INSTALL_COMMAND="$APT_COMMAND_PREFIX apt install $VCPKG_DEPS $COMPILER_DEPS -y"
+INSTALL_COMMAND="$SUDO_PREFIX $PACKAGE_MANAGER_INSTALL_CMD $VCPKG_DEPS $COMPILER_DEPS"
 echo "Running Install Command: $INSTALL_COMMAND"
-$INSTALL_COMMAND || exit 1
+# $INSTALL_COMMAND || exit 1
+$INSTALL_COMMAND
 
 # Update Submodules
 echo "Updating Submodules"
