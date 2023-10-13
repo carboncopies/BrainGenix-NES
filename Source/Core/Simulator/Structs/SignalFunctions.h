169d37a635e79be2d45a1a743fdf7a75a75047bb
Debajyoti Debnath <ddebnath@carboncopies.org>
2023-10-13 15:50:02 -0400

    Add function definition for 1D convolution in SignalFunctions.h
    Also add unittests for Convolve1D.

--- Source/Core/Simulator/Structs/SignalFunctions.h
+++ Source/Core/Simulator/Structs/SignalFunctions.h
@@ -12,13 +12,17 @@
 
 #include <string>
 #include <cmath>
+#include <vector>
 
 namespace BG {
 namespace NES {
 namespace Simulator {
 namespace SignalFunctions {
 
+//! Double exponential expression used in calculation of post-synaptic potential contributions.
 float DoubleExponentExpr(float amp, float tauRise, float tauDecay, float tDiff);
+//! One-dimensional convolution.
+std::vector<float> Convolve1D(std::vector<float> signal, std::vector<float> kernel);
 
 }; // Close Namespace 
 }; // Close Namespace Simulator
