b24f7ba59277fef91801edbf351903cbcb6707a0
Thomas Liao <tliao@carboncopies.org>
2023-10-26 13:15:03 -0700

    Prepare To Use Indices

--- Source/Renderer/BG/Renderer/VulkanHelpers.cpp
+++ Source/Renderer/BG/Renderer/VulkanHelpers.cpp
@@ -45,6 +45,10 @@ const std::vector<Vertex> vertices = {
     {{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}
 };
 
+const std::vector<uint16_t> indices = {
+    0, 1, 2, 2, 3, 0
+};
+
 
 
 bool VulkanInit_CreateInstance(BG::Common::Logger::LoggingSystem* _Logger, RenderData* _RD) {
