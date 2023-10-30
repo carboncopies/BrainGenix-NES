8c9281d1fcf6cd02220c72e700f9dffc1597710a
Thomas Liao <tliao@carboncopies.org>
2023-10-30 16:13:46 -0700

    Create Rotation Test Scene

--- Source/Renderer/BG/Renderer/Interface.cpp
+++ Source/Renderer/BG/Renderer/Interface.cpp
@@ -53,12 +53,12 @@ bool Interface::Initialize(int _NumArgs, char** _ArgValues) {
 
 
     Shaders::Phong CubeShader;
-    CubeShader.DiffuseColor_  = vsg::vec4(0.2f, 0.8f, 0.2f, 1.0f);
+    CubeShader.DiffuseColor_  = vsg::vec4(0.5f, 0.5f, 0.5f, 1.0f);
     CubeShader.SpecularColor_ = vsg::vec4(1.f, 0.1f, 0.1f, 1.0f);
     CubeShader.Type_ = Shaders::SHADER_PHONG;
     Primitive::Cube CreateInfo;
-    CreateInfo.Position_ = vsg::vec3(0.0f, 0.0f, 0.0f);
-    CreateInfo.Rotation_ = vsg::vec3(45.0f, 0.0f, 0.0f);
+    CreateInfo.Position_ = vsg::vec3(0.0f, -1.0f, 0.0f);
+    CreateInfo.Rotation_ = vsg::vec3(0.0f, 0.0f, 0.0f);
     CreateInfo.Scale_    = vsg::vec3(1.0f, 1.0f, 1.0f);
     CreateInfo.Shader_ = &CubeShader;
     AddBox(&CreateInfo);
@@ -73,8 +73,14 @@ bool Interface::Initialize(int _NumArgs, char** _ArgValues) {
     CubeShader.SpecularColor_ = vsg::vec4(0.1f, 1.f, 0.1f, 1.0f);
     AddBox(&CreateInfo);
 
-    CreateInfo.Position_ = vsg::vec3(-1.1f, 0.0f, 0.0f);
+    CreateInfo.Position_ = vsg::vec3(0.0f, 0.0f, 0.0f);
     CreateInfo.Rotation_ = vsg::vec3(0.0f, 45.0f, 0.0f);
+    CubeShader.DiffuseColor_  = vsg::vec4(0.2f, 0.8f, 0.2f, 1.0f);
+    CubeShader.SpecularColor_ = vsg::vec4(0.1f, 1.f, 0.1f, 1.0f);
+    AddBox(&CreateInfo);
+
+    CreateInfo.Position_ = vsg::vec3(-1.1f, 0.0f, 0.0f);
+    CreateInfo.Rotation_ = vsg::vec3(45.0f, 0.0f, 0.0f);
     CubeShader.DiffuseColor_  = vsg::vec4(0.8f, 0.2f, 0.2f, 1.0f);
     CubeShader.SpecularColor_ = vsg::vec4(0.1f, 0.1f, 1.0f, 1.0f);
     AddBox(&CreateInfo);
