b478a505d22f7767a7d00cd6b652d1d5b402d948
Randal A. Koene <rkoene@carboncopies.org>
2023-11-29 18:07:50 -0800

    Added Geometry.h file.

---
+++ Source/Core/Simulator/Structs/Geometry.h
@@ -0,0 +1,24 @@
+#pragma once
+
+namespace BG {
+namespace NES {
+namespace Simulator {
+namespace Shapes {
+
+
+struct BoundingBox {
+    float bb_point1[3];
+    float bb_point2[3];
+};
+
+/**
+ * @brief This is inherited by all shapes.
+ */
+struct Geometry {
+    BoundingBox GetBoundingBox() = 0;
+};
+
+}; // Close Namespace Shapes
+}; // Close Namespace Simulator
+}; // Close Namespace NES
+}; // Close Namespace BG
