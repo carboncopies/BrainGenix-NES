077065ae245a70bb2fbfb96057f13b434dd8fcc4
Jason Wong <jwong@carboncopies.org>
2023-12-20 22:04:55 +0000

    Changed to use logger instead

--- Source/Renderer/BG/Renderer/SceneGraph/Manager.cpp
+++ Source/Renderer/BG/Renderer/SceneGraph/Manager.cpp
@@ -228,7 +228,8 @@ bool Manager::Headless_GetImage(std::string _FilePath) {
         // colorFilename = std::to_string(RenderData_->framenumber) + ".bmp";
         // RenderData_->framenumber+=1;
         bool status = Instance.write(imageData, _FilePath);
-        std::cout<<"Wrote File "<<_FilePath<<std::endl;;
+        logger_ ->Log("Wrote file", 0);
+        //std::cout<<"Wrote File "<<_FilePath<<std::endl;;
         if (!status) {
             std::cout<<"Error writing file!\n";
         }
