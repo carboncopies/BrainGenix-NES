853a4f0b0ac6c13150856f4e8bf316e69586d13a
Thomas Liao <tliao@carboncopies.org>
2024-01-12 16:06:34 -0800

    Fix Formatting

--- Source/Core/Simulator/Structs/RecordingElectrode.test.cpp
+++ Source/Core/Simulator/Structs/RecordingElectrode.test.cpp
@@ -7,19 +7,18 @@
    struct. Additional Notes: None Date Created: 2023-10-13
 */
 
-#include <cmath>
-#include <memory>
-#include <tuple>
-#include <vector>
-
-#include <gtest/gtest.h>
-
 #include <Simulator/BallAndStick/BSAlignedBrainRegion.h>
 #include <Simulator/BallAndStick/BSAlignedNC.h>
 #include <Simulator/Geometries/Box.h>
 #include <Simulator/Geometries/VecTools.h>
 #include <Simulator/Structs/RecordingElectrode.h>
 #include <Simulator/Structs/Simulation.h>
+#include <gtest/gtest.h>
+
+#include <cmath>
+#include <memory>
+#include <tuple>
+#include <vector>
 
 /**
  * @brief Test class for unit tests for the RecordingElectrode struct.
@@ -38,12 +37,7 @@ struct RecordingElectrodeTest : testing::Test {
         testSim = std::make_shared<BG::NES::Simulator::Simulation>(nullptr);  // this will fail an assert when we try and use the logger fyi
         testNC = std::make_shared<BG::NES::Simulator::BallAndStick::BSAlignedNC>(4000, 2);
         auto testBSAlignedNC = std::dynamic_pointer_cast<BG::NES::Simulator::BallAndStick::BSAlignedNC>(testNC);
-        testRegion = std::make_shared<
-            BG::NES::Simulator::BallAndStick::BSAlignedBrainRegion>(3000,
-            std::make_shared<BG::NES::Simulator::Geometries::Box>(
-                BG::NES::Simulator::Geometries::Vec3D(0.0, 0.0, 0.0),
-                BG::NES::Simulator::Geometries::Vec3D(20.0, 20.0, 20.0)),
-            testBSAlignedNC);
+        testRegion = std::make_shared<BG::NES::Simulator::BallAndStick::BSAlignedBrainRegion>(3000, std::make_shared<BG::NES::Simulator::Geometries::Box>(BG::NES::Simulator::Geometries::Vec3D(0.0, 0.0, 0.0), BG::NES::Simulator::Geometries::Vec3D(20.0, 20.0, 20.0)), testBSAlignedNC);
 
         testSim->AddCircuit(testNC);
         testSim->AddRegion(testRegion);
@@ -58,19 +52,15 @@ struct RecordingElectrodeTest : testing::Test {
 
 TEST_F(RecordingElectrodeTest, test_CoordsElectrodeToSystem_default) {
     auto eLocRatio = std::make_tuple(0.0, 0.5, 0.2);
-    auto toAdd =
-        (testElectrode->TipPosition_um - testElectrode->EndPosition_um) *
-        std::get<2>(eLocRatio);
+    auto toAdd = (testElectrode->TipPosition_um - testElectrode->EndPosition_um) * std::get<2>(eLocRatio);
     auto expectedSysLoc_um = testElectrode->TipPosition_um + toAdd;
 
-    ASSERT_TRUE(testElectrode->CoordsElectrodeToSystem(eLocRatio) ==
-                expectedSysLoc_um);
+    ASSERT_TRUE(testElectrode->CoordsElectrodeToSystem(eLocRatio) == expectedSysLoc_um);
 }
 
 TEST_F(RecordingElectrodeTest, test_InitSystemCoordSiteLocations_default) {
     ASSERT_FALSE(testElectrode->SiteLocations_um.empty());
-    ASSERT_EQ(testElectrode->SiteLocations_um.size(),
-              testElectrode->Sites.size());
+    ASSERT_EQ(testElectrode->SiteLocations_um.size(), testElectrode->Sites.size());
 }
 
 TEST_F(RecordingElectrodeTest, test_InitNeuronReferencesAndDistances_default) {
@@ -80,8 +70,7 @@ TEST_F(RecordingElectrodeTest, test_InitNeuronReferencesAndDistances_default) {
 
     expectedSize = testElectrode->SiteLocations_um.size();
     ASSERT_FALSE(testElectrode->NeuronSomaToSiteDistances_um2.empty());
-    ASSERT_EQ(testElectrode->NeuronSomaToSiteDistances_um2.size(),
-              expectedSize);
+    ASSERT_EQ(testElectrode->NeuronSomaToSiteDistances_um2.size(), expectedSize);
 }
 
 TEST_F(RecordingElectrodeTest, test_InitRecords_default) {
@@ -101,8 +90,7 @@ TEST_F(RecordingElectrodeTest, test_ElectricFieldPotential_default) {
 
     // Site index is out of bounds
     try {
-        gotE_mV = testElectrode->ElectricFieldPotential(
-            testElectrode->NeuronSomaToSiteDistances_um2.size());
+        gotE_mV = testElectrode->ElectricFieldPotential(testElectrode->NeuronSomaToSiteDistances_um2.size());
         FAIL();
     } catch (std::out_of_range &ex) {
         ASSERT_STREQ("Out of bounds. (siteIdx)", ex.what());
@@ -114,30 +102,24 @@ TEST_F(RecordingElectrodeTest, test_ElectricFieldPotential_default) {
         gotE_mV = testElectrode->ElectricFieldPotential(0);
         FAIL();
     } catch (std::overflow_error &ex) {
-        ASSERT_STREQ("Cannot divide by zero. (SensitivityDampening)",
-                     ex.what());
+        ASSERT_STREQ("Cannot divide by zero. (SensitivityDampening)", ex.what());
     }
 
     // Correct Site Index and nonzero Sensitivity Dampening
     size_t siteIdx = 0;
     float expectedE_mV = 0.0;
-    auto siteDistances_um =
-        testElectrode->NeuronSomaToSiteDistances_um2[siteIdx];
+    auto siteDistances_um = testElectrode->NeuronSomaToSiteDistances_um2[siteIdx];
 
     testElectrode->SensitivityDampening = 2.0;
 
     for (size_t i = 0; i < testElectrode->Neurons.size(); ++i) {
-        float Vm_mV = std::dynamic_pointer_cast<
-                          BG::NES::Simulator::BallAndStick::BSNeuron>(
-                          testElectrode->Neurons[i])
-                          ->Vm_mV;
+        float Vm_mV = std::dynamic_pointer_cast<BG::NES::Simulator::BallAndStick::BSNeuron>(testElectrode->Neurons[i])->Vm_mV;
 
         if (siteDistances_um[i] <= 1.0)
             expectedE_mV = Vm_mV;
         else {
             expectedE_mV = Vm_mV / siteDistances_um[i];
-            expectedE_mV +=
-                (expectedE_mV / testElectrode->SensitivityDampening);
+            expectedE_mV += (expectedE_mV / testElectrode->SensitivityDampening);
         }
     }
     float lowerLim = expectedE_mV - 0.5 * testElectrode->NoiseLevel;
@@ -150,16 +132,14 @@ TEST_F(RecordingElectrodeTest, test_ElectricFieldPotential_default) {
 TEST_F(RecordingElectrodeTest, test_Record_default) {
     // Nothing has been recorded immediately after setup
     ASSERT_TRUE(testElectrode->TRecorded_ms.empty());
-    for (const auto &E_mVVec : testElectrode->E_mV)
-        ASSERT_TRUE(E_mVVec.empty());
+    for (const auto &E_mVVec : testElectrode->E_mV) ASSERT_TRUE(E_mVVec.empty());
 
     float recordingTime_ms = 1.0;
     testElectrode->Record(recordingTime_ms);
 
     ASSERT_FALSE(testElectrode->TRecorded_ms.empty());
     ASSERT_EQ(testElectrode->TRecorded_ms.back(), recordingTime_ms);
-    for (const auto &E_mVVec : testElectrode->E_mV)
-        ASSERT_FALSE(E_mVVec.empty());
+    for (const auto &E_mVVec : testElectrode->E_mV) ASSERT_FALSE(E_mVVec.empty());
 }
 
 TEST_F(RecordingElectrodeTest, test_GetRecording_default) {
@@ -168,8 +148,7 @@ TEST_F(RecordingElectrodeTest, test_GetRecording_default) {
     // No data immediately after setup
     auto it = data.find("E_mV");
     ASSERT_TRUE(it != data.end());
-    for (const auto &E_mVVec : data.at("E_mV"))
-        ASSERT_TRUE(E_mVVec.empty());
+    for (const auto &E_mVVec : data.at("E_mV")) ASSERT_TRUE(E_mVVec.empty());
 
     // Data available after recording for some time.
     testElectrode->Record(0.1);
