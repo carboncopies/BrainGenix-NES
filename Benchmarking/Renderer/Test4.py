a6782cbed39d8d77270574e09ce901661e967a02
Thomas Liao <tliao@carboncopies.org>
2024-01-19 15:01:45 -0800

    Update Test4 Benchmark

--- Benchmarking/Renderer/Test4.py
+++ Benchmarking/Renderer/Test4.py
@@ -148,7 +148,7 @@ def main():
 
     # Setup VSDA Renderer
     EMConfig = NES.VSDA.EM.Configuration()
-    EMConfig.PixelResolution_nm = 0.1
+    EMConfig.PixelResolution_nm = 0.05
     EMConfig.ImageWidth_px = 512
     EMConfig.ImageHeight_px = 512
     EMConfig.SliceThickness_nm = 100
