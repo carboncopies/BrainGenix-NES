8c15b2137e360c45d33a49deddfcf4ff88387ded
Thomas Liao <tliao@carboncopies.org>
2024-01-25 00:59:41 -0800

    Update Deps

--- Tools/Setup.sh
+++ Tools/Setup.sh
@@ -47,7 +47,7 @@ VCPKG_DEPS="curl zip unzip tar pkg-config autoconf flex bison"
 if [ "$DISTRO" = "arch" ]; then
     VULKAN_DEPS="vulkan-devel"
 else
-    VULKAN_DEPS="libvulkan-dev vulkan-validationlayers-dev vulkan-tools libxcb-xfixes0-dev"  
+    VULKAN_DEPS="libvulkan-dev vulkan-validationlayers-dev vulkan-tools libxcb-xfixes0-dev libx11-dev libxrandr-dev"  
 fi
 
 # Install Everything
