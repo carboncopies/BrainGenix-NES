c692b26caf56ad17e718eb5656e409a5bdbe0801
Thomas Liao <tliao@carboncopies.org>
2024-01-26 00:40:09 -0800

    Fix Status Info

--- Source/Core/VSDA/EMRenderer.cpp
+++ Source/Core/VSDA/EMRenderer.cpp
@@ -100,7 +100,7 @@ bool ExecuteSubRenderOperations(BG::Common::Logger::LoggingSystem* _Logger, Simu
     _Logger->Log("Rendering " + std::to_string(SubRegions.size()) + " Sub Regions", 4);
     for (size_t i = 0; i < SubRegions.size(); i++) {
         EMRenderSubRegion(_Logger, &SubRegions[i], _ImageProcessorPool, _GeneratorPool);
-        _Simulation->VSDAData_.CurrentRegion_ = i;
+        _Simulation->VSDAData_.CurrentRegion_ = i + 1;
     }
 
 
