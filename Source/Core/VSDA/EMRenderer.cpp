9093d66654cf511cab623b48fdf45f4b3d505a32
Thomas Liao <tliao@carboncopies.org>
2024-01-30 17:36:31 -0800

    Implement First Draft Of ImageOverlap Fix

--- Source/Core/VSDA/EMRenderer.cpp
+++ Source/Core/VSDA/EMRenderer.cpp
@@ -43,42 +43,86 @@ bool ExecuteSubRenderOperations(BG::Common::Logger::LoggingSystem* _Logger, Simu
     // -- Phase 1 --
     // We're going to now generate a list of regions which can then be rendered on this machine
     // to do so, we're going to use a configurable max_voxel_array_dimension to find our max size at this resolution, and then calculate the bounding box based on that
-    size_t MaxVoxelArrayAxisSize = 3000; // Hard-coded for now, will eventually do this based on hardware limitations
+    size_t MaxVoxelArrayAxisSize = 600; // Hard-coded for now, will eventually do this based on hardware limitations
+    size_t MaxVoxelArrayAxisSize_vox = MaxVoxelArrayAxisSize;
 
-    // Now, we're going to calculate the max size of the voxel array in microns on axis given this max voxel count and our VSDAData voxel size parameter
-    double SubRegionStepSize_um = (double)MaxVoxelArrayAxisSize * Params->VoxelResolution_um;
+    // Calculate Subregion Coordinates that allign with the image step size, that way we don't have any issues where there are gaps in the middle of the image stack.
+    // Please note that we're currently assuming images are square, and will probably break if they're not.
+    // See the readme.md file in this directory for more information regarding the algorithm and visualizations.
+
+    // Firstly, we calculate the size that each image will be advanced on each subregion.
+    // This way, we can calculate the total size of our base region in terms of number of images.
+    double ImageStepSizeX_um = (Params->ImageWidth_px / Params->NumPixelsPerVoxel_px) * Params->VoxelResolution_um * (1 - (double(Params->ScanRegionOverlap_percent) / 100.));
+    double ImageStepSizeY_um = (Params->ImageHeight_px / Params->NumPixelsPerVoxel_px) * Params->VoxelResolution_um * (1 - (double(Params->ScanRegionOverlap_percent) / 100.));
+    int NumImagesInXDimension_img = ceil(BaseRegion->SizeX() / ImageStepSizeX_um);
+    int NumImagesInYDimension_img = ceil(BaseRegion->SizeX() / ImageStepSizeY_um);
+
+    // Nextly, we're going to figure out the step size information for the subregions.
+    // This will enable us to create subregions that exactly are multiples of the image step sizes, removing any overlap (unless it's on the border, then it may overshoot slightly)
+    double MaxVoxelArraySize_um = MaxVoxelArrayAxisSize_vox * Params->VoxelResolution_um;
+    double SubRegionStepSizeX_um = floor(MaxVoxelArraySize_um / ImageStepSizeX_um) * ImageStepSizeX_um;
+    double SubRegionStepSizeY_um = floor(MaxVoxelArraySize_um / ImageStepSizeY_um) * ImageStepSizeY_um;
+    double SubRegionStepSizeZ_um = MaxVoxelArrayAxisSize * Params->VoxelResolution_um;
     
-    // Next, we identify the number of regions needed in each direction
-    int NumXSteps = ceil(BaseRegion->SizeX() / SubRegionStepSize_um);
-    int NumYSteps = ceil(BaseRegion->SizeY() / SubRegionStepSize_um);
-    int NumZSteps = ceil(BaseRegion->SizeZ() / SubRegionStepSize_um);
+
+    // Okay, now that we know how big each step for our subregion should be at maximum, we can now calculate the amount they need to overlap in each direction.
+    // Note that Z is omitted since we don't overlap slices, just on the x,y axis not z.
+    double SubRegionOverlapX_um = (Params->ImageWidth_px / Params->NumPixelsPerVoxel_px) * Params->VoxelResolution_um * (double(Params->ScanRegionOverlap_percent) / 100.);
+    double SubRegionOverlapY_um = (Params->ImageHeight_px / Params->NumPixelsPerVoxel_px) * Params->VoxelResolution_um * (double(Params->ScanRegionOverlap_percent) / 100.);
+
+    // Next, we can calculate the number of regions in each dimension
+    // Then we will enumerate them later on and calculate the bottom left and top right point for each
+    int NumSubRegionsInXDim = ceil(BaseRegion->SizeX() / SubRegionStepSizeX_um);
+    int NumSubRegionsInYDim = ceil(BaseRegion->SizeY() / SubRegionStepSizeY_um);
+    int NumSubRegionsInZDim = ceil(BaseRegion->SizeZ() / SubRegionStepSizeZ_um);
 
 
-    // Finally, we create the SubRegion array and fill it up with this info
+    // Now, we go through all of the steps in each direction that we identified, and calculate the bounding boxes for each
     std::vector<SubRegion> SubRegions;
-    for (int XStep = 0; XStep < NumXSteps; XStep++) {
-        for (int YStep = 0; YStep < NumYSteps; YStep++) {
-            for (int ZStep = 0; ZStep < NumZSteps; ZStep++) {
+    for (int XStep = 0; XStep < NumSubRegionsInXDim; XStep++) {
+        for (int YStep = 0; YStep < NumSubRegionsInYDim; YStep++) {
+            for (int ZStep = 0; ZStep < NumSubRegionsInZDim; ZStep++) {
 
-                // Calculate offsets/points for this region
-                double OffsetX_um = XStep * SubRegionStepSize_um;
-                double OffsetY_um = YStep * SubRegionStepSize_um;
-                double OffsetZ_um = ZStep * SubRegionStepSize_um;
+                // -- Phase 2 --
+                // Now, we generate all of the subregions, which is done in this loop.
+                // These need to be placed perfectly so that they line up with images from other subregions
+
+                // Now, we need to calculate the offsets for this region
+                // This is done since the base region is not guarenteed to be positioned at 0,0,0, it could be placed just about anywhere
+                // So, we shift all of our subregions to ensure that they reflect this offset of the base region
+                double BaseRegionOffsetX_um = BaseRegion->Point1X_um;
+                double BaseRegionOffsetY_um = BaseRegion->Point1Y_um;
+                double BaseRegionOffsetZ_um = BaseRegion->Point1Z_um;
                 int LayerOffset = ZStep * MaxVoxelArrayAxisSize;
 
-                // Now we fill in the region with the parameters for this part
+                
+                // Next, we're going to calculate the subregion start and ending boxes
+                // This is the bottom left coordinate for the subregion
+                double SubRegionStartX_um = (XStep * SubRegionStepSizeX_um) + BaseRegionOffsetX_um;
+                double SubRegionStartY_um = (YStep * SubRegionStepSizeY_um) + BaseRegionOffsetY_um;
+                double SubRegionStartZ_um = (ZStep * SubRegionStepSizeZ_um) + BaseRegionOffsetZ_um;
+
+                // And this is the top right coordinate for the subregion
+                // The std::min stuff is to ensure that if we're on the edge of the base region, we don't exceed the user-defined limit
+                // In other words, the std::min is to account for the ceil we took earlier with the NumSubRegionsInDim 
+                double SubRegionEndX_um = std::min((XStep + 1) * SubRegionStepSizeX_um + SubRegionOverlapX_um, (double)BaseRegion->Point2X_um);
+                double SubRegionEndY_um = std::min((YStep + 1) * SubRegionStepSizeY_um + SubRegionOverlapY_um, (double)BaseRegion->Point2Y_um);
+                double SubRegionEndZ_um = std::min((ZStep + 1) * SubRegionStepSizeZ_um, (double)BaseRegion->Point2Z_um);
+
+
+                // Now we can just fill in the structs for this subregion as shown, and append it to the list of subregions to be rendered
                 ScanRegion ThisRegion;
-                ThisRegion.Point1X_um = OffsetX_um + BaseRegion->Point1X_um;
-                ThisRegion.Point1Y_um = OffsetY_um + BaseRegion->Point1Y_um;
-                ThisRegion.Point1Z_um = OffsetZ_um + BaseRegion->Point1Z_um;
-                ThisRegion.Point2X_um = std::min(OffsetX_um + SubRegionStepSize_um, (double)BaseRegion->Point2X_um);
-                ThisRegion.Point2Y_um = std::min(OffsetY_um + SubRegionStepSize_um, (double)BaseRegion->Point2Y_um);
-                ThisRegion.Point2Z_um = std::min(OffsetZ_um + SubRegionStepSize_um, (double)BaseRegion->Point2Z_um);
+                ThisRegion.Point1X_um = SubRegionStartX_um;
+                ThisRegion.Point1Y_um = SubRegionStartY_um;
+                ThisRegion.Point1Z_um = SubRegionStartZ_um;
+                ThisRegion.Point2X_um = SubRegionEndX_um;
+                ThisRegion.Point2Y_um = SubRegionEndY_um;
+                ThisRegion.Point2Z_um = SubRegionEndZ_um;
 
                 SubRegion ThisSubRegion;
                 ThisSubRegion.Sim = _Simulation;
-                ThisSubRegion.RegionOffsetX_um = XStep * SubRegionStepSize_um;
-                ThisSubRegion.RegionOffsetY_um = YStep * SubRegionStepSize_um;
+                ThisSubRegion.RegionOffsetX_um = SubRegionStartX_um; // We should get rid of these
+                ThisSubRegion.RegionOffsetY_um = SubRegionStartY_um; // We should get rid of these
                 ThisSubRegion.LayerOffset = ZStep * MaxVoxelArrayAxisSize;
                 ThisSubRegion.Region = ThisRegion;
 
@@ -91,10 +135,11 @@ bool ExecuteSubRenderOperations(BG::Common::Logger::LoggingSystem* _Logger, Simu
         }
     }
 
+
     _Simulation->VSDAData_.TotalRegions_ = SubRegions.size();
 
 
-    // -- Phase 2 -- 
+    // -- Phase 3 -- 
     // Now, we're just going to go and render each of the different regions
     // This is done through simply running a for loop, and calling the rendersubregion code on each
     _Logger->Log("Rendering " + std::to_string(SubRegions.size()) + " Sub Regions", 4);
