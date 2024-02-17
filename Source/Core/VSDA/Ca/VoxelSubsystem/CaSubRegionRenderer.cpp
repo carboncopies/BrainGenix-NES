bd9c248489220039ef4411a2ee843c8c2e66e9f7
Thomas Liao <tliao@carboncopies.org>
2024-02-17 03:21:41 -0800

    Fix Voxel Starting Index Offset

--- Source/Core/VSDA/Ca/VoxelSubsystem/CaSubRegionRenderer.cpp
+++ Source/Core/VSDA/Ca/VoxelSubsystem/CaSubRegionRenderer.cpp
@@ -100,7 +100,7 @@ bool CaRenderSubRegion(BG::Common::Logger::LoggingSystem* _Logger, SubRegion* _S
     for (unsigned int i = 0; i < NumSlices; i++) {
         std::string FileNamePrefix = "Simulation" + std::to_string(Sim->ID) + "/Calcium/Region" + std::to_string(CaData_->ActiveRegionID_);
 
-        unsigned int CurrentSlice = i * CaData_->Params_.NumVoxelsPerSlice;
+        unsigned int CurrentSlice = (i + 1) * CaData_->Params_.NumVoxelsPerSlice;
         std::vector<std::string> Files = CaRenderSliceFromArray(_Logger, _SubRegion->MaxImagesX, _SubRegion->MaxImagesY, &Sim->CaData_, CaData_->Array_.get(), FileNamePrefix, CurrentSlice, _ImageProcessorPool, XOffset, YOffset, SliceOffset);
         for (size_t i = 0; i < Files.size(); i++) {
             CaData_->RenderedImagePaths_[CaData_->ActiveRegionID_].push_back(Files[i]);
