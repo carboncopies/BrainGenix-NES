78dc86058ba8b7bbc042c2f141f1ed3b5ed7efa3
Debajyoti Debnath <ddebnath@purdue.edu>
2024-03-17 17:55:23 -0400

    Modify CMakeLists files to include libsonata and appropriate third party includes.

---
+++ Source/Core/IO/SonataLoader.h
@@ -0,0 +1,24 @@
+//=================================================================//
+// This file is part of the BrainGenix-NES Neuron Emulation System //
+//=================================================================//
+
+/*
+    Description:
+        This header file serves as the main entry point for the BrainGenix-NES Neuron Emulation System.
+        It includes essential libraries and sets up the main function for the system.
+
+    Additional Notes:
+        None
+
+    Date Created:
+        2024-03-14
+*/
+
+#pragma once
+
+// Standard Libraries (BG convention: use <> instead of "")
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+#include <bbp/sonata/nodes.h>
+
+// Internal Libraries (BG convention: use <> instead of "")
