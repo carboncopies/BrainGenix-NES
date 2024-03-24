906fbdbbb80517a09fced5917afc18ee48c4618e
Debajyoti Debnath <ddebnath@carboncopies.org>
2024-03-24 18:08:52 -0400

    Basic reading of node and edge storage files.

--- Source/Core/IO/SonataLoader.cpp
+++ Source/Core/IO/SonataLoader.cpp
@@ -1,1 +1,25 @@
 #include "IO/SonataLoader.h"
+
+namespace BG {
+namespace NES {
+namespace IO {
+SonataLoader::SonataLoader(const std::string &_nodeFilePath,
+                           const std::string &_edgeFilePath) {
+    nodes = std::make_shared<bbp::sonata::NodeStorage>(_nodeFilePath);
+    edges = std::make_shared<bbp::sonata::EdgeStorage>(_edgeFilePath);
+
+    std::cout << "Node populations:\n";
+    for (const auto &name : nodes->populationNames()) {
+        auto pop = nodes->openPopulation(name);
+        std::cout << name << ": " << pop->size() << "\n";
+    }
+
+    std::cout << "Edge populations:\n";
+    for (const auto &name : edges->populationNames()) {
+        auto pop = edges->openPopulation(name);
+        std::cout << name << ": " << pop->size() << "\n";
+    }
+}
+} // namespace IO
+} // namespace NES
+} // namespace BG
--- Source/Core/IO/SonataLoader.h
+++ Source/Core/IO/SonataLoader.h
@@ -4,8 +4,6 @@
 
 /*
     Description:
-        This header file serves as the main entry point for the BrainGenix-NES Neuron Emulation System.
-        It includes essential libraries and sets up the main function for the system.
 
     Additional Notes:
         None
@@ -17,8 +15,28 @@
 #pragma once
 
 // Standard Libraries (BG convention: use <> instead of "")
+#include <string>
 
 // Third-Party Libraries (BG convention: use <> instead of "")
+#include <bbp/sonata/edges.h>
 #include <bbp/sonata/nodes.h>
 
 // Internal Libraries (BG convention: use <> instead of "")
+
+namespace BG {
+namespace NES {
+namespace IO {
+class SonataLoader {
+  public:
+    //! Nodes loaded from a H5 file containing node population information.
+    std::shared_ptr<bbp::sonata::NodeStorage> nodes;
+    //! Edges loaded from a H5 file containing edge population information.
+    std::shared_ptr<bbp::sonata::EdgeStorage> edges;
+
+    //! Constructor
+    SonataLoader(const std::string &_nodeFilePath,
+                 const std::string &_edgeFilePath);
+};
+} // namespace IO
+} // namespace NES
+} // namespace BG
