511fd8478e5e8d5078d7e91a2c180f3f17dec806
Debajyoti Debnath <ddebnath@purdue.edu>
2024-04-09 18:06:36 -0400

    Modify entrypoint for testing loader.

--- Source/Core/Main.cpp
+++ Source/Core/Main.cpp
@@ -1,55 +1,15 @@
 #include <Main.h>
 
-
 /**
  * @brief The main entry point for the BrainGenix-NES Neuron Emulation System.
- * 
- * This function initializes the system components, including configuration, logging, API server,
- * rendering pool, and simulator. It also prints the BrainGenix logo to the console and runs
- * the main loop to keep the servers running.
- * 
+ *
  * @param NumArguments The number of command-line arguments.
  * @param ArgumentValues An array containing the command-line arguments.
  * @return int The exit code of the program.
  */
-int main(int NumArguments, char** ArgumentValues) {
+int main(int argc, char **argv) {
 
-    // Startup With Config Manager, Will Read Args And Config File, Then Parse Into Config Struct
-    BG::NES::Config::Manager ConfigManager(NumArguments, ArgumentValues);
-    BG::NES::Config::Config& SystemConfiguration = ConfigManager.GetConfig();
-
-    // Setup Logging System
-    BG::Common::Logger::LoggingSystem Logger;
-    Logger.SetKeepVectorLogs(false);
-
-    // Setup API Server
-    BG::NES::API::RPCManager APIManager(&SystemConfiguration, &Logger);
-
-    // Setup RenderPools
-    BG::NES::Simulator::VSDA::RenderPool RenderPool(&Logger, false, 5);
-    BG::NES::Simulator::VisualizerPool VisualizerPool(&Logger, false, 1);
-
-    // Setup Simulator (Adding the routes here)
-    BG::NES::Simulator::SimulationRPCInterface SimulationRPCInterface(&Logger, &SystemConfiguration, &RenderPool, &VisualizerPool, &APIManager);
-    BG::NES::Simulator::GeometryRPCInterface   GeometryRPCInterface(&Logger, SimulationRPCInterface.GetSimulationVectorPtr(), &APIManager);
-    BG::NES::Simulator::ModelRPCInterface      ModelRPCInterface(&Logger, SimulationRPCInterface.GetSimulationVectorPtr(), &APIManager);
-    BG::NES::Simulator::VisualizerRPCInterface VisualizerRPCInterface(&Logger, SimulationRPCInterface.GetSimulationVectorPtr(), &APIManager);
-    BG::NES::Simulator::VSDA::VSDARPCInterface VSDARPCInterface(&Logger, &APIManager, SimulationRPCInterface.GetSimulationVectorPtr());
-
-    // Print ASCII BrainGenix Logo To Console
-    BG::NES::Util::LogLogo(&Logger);
-
-
-    // Check if we have profiling enabled, if so do that then quit
-    if (SystemConfiguration.ProfilingStatus_ != BG::NES::Config::PROFILE_NONE) {
-        BG::NES::Profiling::Manager(&Logger, &SystemConfiguration, &SimulationRPCInterface, &RenderPool, &VisualizerPool, &APIManager);
-        return 0;
-    }
-
-    // Block forever while servers are running
-    while (true) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
-    }
+    BG::NES::IO::SonataLoader loader(argv[1], argv[2]);
 
     return 0;
 }
--- Source/Core/Main.h
+++ Source/Core/Main.h
@@ -3,22 +3,22 @@
 //=================================================================//
 
 /*
-    Description: 
+    Description:
         This header file serves as the main entry point for the BrainGenix-NES Neuron Emulation System.
         It includes essential libraries and sets up the main function for the system.
 
-    Additional Notes: 
+    Additional Notes:
         None
 
-    Date Created: 
+    Date Created:
         2023-06-24
 */
 
 #pragma once
 
 // Standard Libraries (BG convention: use <> instead of "")
-#include <iostream>
 #include <functional>
+#include <iostream>
 
 // Third-Party Libraries (BG convention: use <> instead of "")
 #include <rpc/server.h>
@@ -26,28 +26,30 @@
 // Internal Libraries (BG convention: use <> instead of "")
 #include <BG/Common/Logger/Logger.h>
 #include <BG/Renderer/Interface.h>
-#include <Config/ConfigurationManager.h>
 #include <Config/Config.h>
+#include <Config/ConfigurationManager.h>
 #include <RPC/RPCManager.h>
 
-#include <Simulator/RPC/SimulationRPCInterface.h>
 #include <Simulator/RPC/GeometryRPCInterface.h>
 #include <Simulator/RPC/ModelRPCInterface.h>
+#include <Simulator/RPC/SimulationRPCInterface.h>
 #include <VSDA/VSDARPCInterface.h>
 #include <Visualizer/VisualizerRPCInterface.h>
 
+#include <Profiling/ProfilingManager.h>
+#include <Util/LogLogo.h>
 #include <VSDA/RenderPool.h>
 #include <Visualizer/VisualizerPool.h>
-#include <Util/LogLogo.h>
-#include <Profiling/ProfilingManager.h>
+
+#include <IO/SonataLoader.h>
 
 /**
  * @brief The main entry point for the BrainGenix-NES Neuron Emulation System.
- * 
+ *
  * This function initializes the system, sets up essential components, and starts the main loop.
- * 
+ *
  * @param NumArguments The number of command-line arguments.
  * @param ArgumentValues An array containing the command-line arguments.
  * @return int The exit code of the program.
  */
-int main(int NumArguments, char** ArgumentValues);
+int main(int NumArguments, char **ArgumentValues);
