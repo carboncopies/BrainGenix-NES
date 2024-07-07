ecf6255bc4e74edb69c5baef7cbbb1b6df176d7c
Randal A. Koene <rkoene@carboncopies.org>
2024-07-06 20:18:13 -0700

    Added linking of messaging glue for Netmorph to NES logging.

--- Source/Core/Netmorph/NetmorphManagerThread.cpp
+++ Source/Core/Netmorph/NetmorphManagerThread.cpp
@@ -5,22 +5,44 @@
 
 // Standard Libraries (BG convention: use <> instead of "")
 #include <vector>
+#include <memory>
 
 // Third-Party Libraries (BG convention: use <> instead of "")
 #include <Include/Netmorph.h>
+#include <Include/Embeddable.h>
 
 // Internal Libraries (BG convention: use <> instead of "")
 #include <Netmorph/NetmorphManagerThread.h>
-
-
-
 
 
 namespace BG {
 namespace NES {
 namespace Simulator {
 
+class Nm2NESLogging: public Netmorph2NESLogging {
+protected:
+    BG::Common::Logger::LoggingSystem& logger;
+public:
+    Nm2NESLogging(BG::Common::Logger::LoggingSystem& _logger): logger(_logger) {}
+
+    virtual void error(const std::string & msg) {
+        logger.Log(msg, 7);
+    }
+    virtual void warning(const std::string & msg) {
+        logger.Log(msg, 6);
+    }
+    virtual void report(const std::string & msg) {
+        logger.Log(msg, 3);
+    }
+    virtual void progress(const std::string & msg) {
+        logger.Log(msg, 5);
+    }
+};
+
 int ExecuteNetmorphOperation(BG::Common::Logger::LoggingSystem* _Logger, NetmorphParameters* _Params) {
+
+    // Create link to redirect messaging from Netmorph to the NES Logger.
+    embedlog = std::make_unique<Nm2NESLogging>(*_Logger);
 
     _Logger->Log("Starting Netmorph Simulation", 5);
     _Params->Result = Netmorph(&_Params->Progress_percent, _Params->ModelFile);
