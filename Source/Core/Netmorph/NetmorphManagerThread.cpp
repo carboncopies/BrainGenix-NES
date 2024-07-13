2272d4cf6b7105d1e1e778953c49cd9f19ce5bb3
Randal A. Koene <rkoene@carboncopies.org>
2024-07-13 11:02:47 -0700

    Fix.

--- Source/Core/Netmorph/NetmorphManagerThread.cpp
+++ Source/Core/Netmorph/NetmorphManagerThread.cpp
@@ -47,14 +47,6 @@ public:
     }
 };
 
-// --- The following may be moved
-// The following macros are used for easy access to linked lists built
-// in the manner of 2009 Netmorph.
-// #define PLL_LOOP_FORWARD(PLLType,Init_El,Extra_Test) \
-//     for (PLLType * e = Init_El; ((e) && (Extra_Test)); e = e->Next())
-// #define PLL_LOOP_FORWARD_NESTED(PLLType,Init_El,Extra_Test,RefVar) \
-//     for (PLLType * RefVar = Init_El; ((RefVar) && (Extra_Test)); RefVar = RefVar->Next())
-
 #define NETMORPH_PARAMS_FAIL(msg) { \
         _Params.Sim->Logger_->Log(msg, 7); \
         _Params.Result.Status = false; \
@@ -144,74 +136,6 @@ public:
     }
 };
 
-// bool RecursiveNeuriteBuild(bool IsAxon, NetmorphParameters& _Params, CoreStructs::SCNeuronStruct& N, fibre_segment* fsptr) {
-
-//     //*** MAYBE FIX REPEATED NAMES
-
-//     // Build neurite cylinder.
-//     Geometries::Cylinder S;
-//     S.End1Radius_um = fsptr->Diameter() <= 0.0 ? 1.0 : fsptr->Diameter();
-//     if (!fsptr->Parent()) {
-//         S.End0Radius_um = S.End1Radius_um;
-//     } else {
-//         S.End0Radius_um = fsptr->Parent()->Diameter();
-//         if (S.End0Radius_um <= 0.0) S.End0Radius_um = S.End1Radius_um;
-//     }
-//     S.End0Pos_um.x = fsptr->P0.X();
-//     S.End0Pos_um.y = fsptr->P0.Y();
-//     S.End0Pos_um.z = fsptr->P0.Z();
-//     S.End1Pos_um.x = fsptr->P1.X();
-//     S.End1Pos_um.y = fsptr->P1.Y();
-//     S.End1Pos_um.z = fsptr->P1.Z();
-//     S.Name = "cyl-"+N.Name;
-
-//     S.ID = _Params.Sim->AddCylinder(S);
-
-//     // Build neurite compartment.
-//     Compartments::BS C;
-//     C.ShapeID = S.ID;
-//     C.MembranePotential_mV = N.MembranePotential_mV;
-//     C.RestingPotential_mV = N.RestingPotential_mV;
-//     C.SpikeThreshold_mV = N.SpikeThreshold_mV;
-//     C.AfterHyperpolarizationAmplitude_mV = N.AfterHyperpolarizationAmplitude_mV;
-//     C.DecayTime_ms = N.DecayTime_ms;
-//     if (IsAxon) {
-//         C.Name = "axon-"+N.Name;
-//     } else {
-//         C.Name = "dendcomp-"+N.Name;
-//     }
-//     if (_Params.Sim->AddSCCompartment(C)<0) {
-//         NETMORPH_PARAMS_FAIL("RecursiveDendriteBuild failed: Missing Cylinder for neurite compartment build.");
-//         return false;
-//     }
-
-//     if (IsAxon) {
-//         N.AxonCompartmentIDs.emplace_back(C.ID);
-//     } else {
-//         N.DendriteCompartmentIDs.emplace_back(C.ID);
-//     }
-
-//     if ((!fsptr->Branch1()) && (!fsptr->Branch2())) {
-//         return true; // This is a terminal branch.
-//     }
-
-//     //SegmentIDMap.emplace(fsptr, C.ID);
-//     fsptr->cache.i = C.ID;
-
-//     //bool EndIsBifurcation = (Branch1() && Branch2()); // *** We may find this useful to know later.
-
-//     if (fsptr->Branch1()) if (!RecursiveNeuriteBuild(IsAxon, _Params, N, fsptr->Branch1())) {
-//         NETMORPH_PARAMS_FAIL("RecursiveDendriteBuild failed on Branch1.");
-//         return false;
-//     }
-
-//     if (fsptr->Branch2()) if (!RecursiveNeuriteBuild(IsAxon, _Params, N, fsptr->Branch2())) {
-//         NETMORPH_PARAMS_FAIL("RecursiveDendriteBuild failed on Branch2.");
-//         return false;
-//     }
-//     return true;
-// }
-
 const std::map<synapse_type, float> conductances_nS = {
     { syntype_AMPAR, 40.0 },
     { syntype_NMDAR, 60.0 },
@@ -269,19 +193,6 @@ bool SynapsesBuild(NetmorphParameters& _Params, const PSPTiming& psp_timing, syn
         NETMORPH_PARAMS_FAIL("SynapsesBuild failed: Postsynaptic neurite segment not found.");
         return false;
     }
-
-    // auto it_presegment = SegmentIDMap.find(syn.Structure()->AxonSegment());
-    // if (it_presegment==SegmentIDMap.end()) {
-    //     NETMORPH_PARAMS_FAIL("SynapsesBuild failed: Presynaptic neurite segment not found.");
-    //     return false;
-    // }
-    // C.SourceCompartmentID  = it_presegment->second;
-    // auto it_postsegment = SegmentIDMap.find(syn.Structure()->DendriteSegment());
-    // if (it_postsegment==SegmentIDMap.end()) {
-    //     NETMORPH_PARAMS_FAIL("SynapsesBuild failed: Postsynaptic neurite segment not found.");
-    //     return false;
-    // }
-    // C.DestinationCompartmentID = it_postsegment->second;
 
     C.ShapeID = S.ID;
 
@@ -368,13 +279,6 @@ bool BuildFromNetmorphNetwork(NetmorphParameters& _Params) {
     Net.tree_op(unparsed_pre);
     unparsed_pre.log();
 
-    // // The SegmentIDMap is used to map Netmorph neurite fibre segments to
-    // // NES compartment IDs. This is needed when building synapse receptor objects.
-    // // Note that there are no segments mapped to soma compartment IDs, because
-    // // Netmorph is exclusively involved with neurite outgrowth and does not define
-    // // soma morphology.
-    // std::map<fibre_segment*, int> SegmentIDMap;
-
     PLL_LOOP_FORWARD(neuron, Net.PLLRoot<neuron>::head(), 1) {
 
         // 0. Prepare neuron scaffold with lists.
@@ -423,24 +327,12 @@ bool BuildFromNetmorphNetwork(NetmorphParameters& _Params) {
         NeuriteBuild dendrites_build(false, _Params, N);
         e->tree_op(dendrites_build);
         dendrites_build.logerrors();
-        // PLL_LOOP_FORWARD_NESTED(fibre_structure, e->InputStructure()->head(), 1, dptr) {
-        //     if (!RecursiveNeuriteBuild(false, _Params, N, dptr)) {
-        //         NETMORPH_PARAMS_FAIL("BuildFromNetmorphNetwork failed: Error in dendrite build.");
-        //         return false;
-        //     }
-        // }
 
         // 4. Build axons.
         parsing_fs_type = axon_fs;
         NeuriteBuild axons_build(true, _Params, N);
         e->tree_op(axons_build);
         axons_build.logerrors();
-        // PLL_LOOP_FORWARD_NESTED(fibre_structure, e->OutputStructure()->head(), 1, aptr) {
-        //     if (!RecursiveNeuriteBuild(true, _Params, N, aptr)) {
-        //         NETMORPH_PARAMS_FAIL("BuildFromNetmorphNetwork failed: Error in axon build.");
-        //         return false;
-        //     }
-        // }
 
         // 5. Build neurons.
         if (_Params.Sim->AddSCNeuron(N)<0) {
