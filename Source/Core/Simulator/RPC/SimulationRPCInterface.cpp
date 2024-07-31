4025e23942da27100a3ae09c6f818f7a73d26fa4
Randal A. Koene <rkoene@carboncopies.org>
2024-07-31 00:20:34 -0700

    More fixes.

--- Source/Core/Simulator/RPC/SimulationRPCInterface.cpp
+++ Source/Core/Simulator/RPC/SimulationRPCInterface.cpp
@@ -317,7 +317,7 @@ std::string SimulationRPCInterface::SetPrePostStrength(std::string _JSONRequest)
     if ((!Handle.GetParInt("PreSyn", PreSyn)) ||
         (!Handle.GetParInt("PostSyn", PostSyn)) ||
         (!Handle.GetParFloat("Conductance_nS", Conductance_nS))) {
-        return Handle>ErrResponse();
+        return Handle.ErrResponse();
     }
 
     if (!Handle.Sim()->UpdatePrePostStrength(PreSyn, PostSyn, Conductance_nS)) {
@@ -336,7 +336,7 @@ std::string SimulationRPCInterface::SetAllStrength(std::string _JSONRequest) {
 
     float Conductance_nS;
     if (!Handle.GetParFloat("Conductance_nS", Conductance_nS)) {
-        return Handle>ErrResponse();
+        return Handle.ErrResponse();
     }
 
     Handle.Sim()->UpdateAllStrength(Conductance_nS);
@@ -356,7 +356,7 @@ std::string SimulationRPCInterface::BatchSetPrePostStrength(std::string _JSONReq
     if ((!Handle.GetParVecInt("PreSyn", PreSyn)) ||
         (!Handle.GetParVecInt("PostSyn", PostSyn)) ||
         (!Handle.GetParVecFloat("Conductance_nS", Conductance_nS))) {
-        return Handle>ErrResponse();
+        return Handle.ErrResponse();
     }
 
     if (!Handle.Sim()->UpdateBatchPrePostStrength(PreSyn, PostSyn, Conductance_nS)) {
