4f14dec991ea793ed2b3daeae25893b7b87ccf83
Randal A. Koene <rkoene@carboncopies.org>
2024-07-31 20:48:30 -0700

    Fix.

--- Source/Core/Netmorph/NetmorphManagerThread.cpp
+++ Source/Core/Netmorph/NetmorphManagerThread.cpp
@@ -423,7 +423,7 @@ bool BuildFromNetmorphNetwork(NetmorphParameters& _Params) {
     Net.synapse_op(synapse_build);
     synapse_build.logerrors();
 
-    RegionBuild region_build;
+    RegionBuild region_build(_Params);
     Net.region_op(region_build);
     region_build.logerrors();
 
