7da68461903ac4bdcda81af148e6558c14406276
Randal A. Koene <rkoene@carboncopies.org>
2025-08-09 07:06:59 -0700

    Adding triangulation option for spike times and fixing a bug.

--- Source/Core/Simulator/LIFCompartmental/LIFCNeuron.cpp
+++ Source/Core/Simulator/LIFCompartmental/LIFCNeuron.cpp
@@ -112,7 +112,7 @@ void LIFCNeuron::spike(float tfire) {
     } else {
         if (build_data.ResetMethod == CoreStructs::TOVM) {
             VReset_mV = Vm_mV; // Remember value before AP
-            VReset_mV = std::min(Vm_mV, V_th_adaptive);
+            VReset_mV = std::min(Vm_mV, VAct_mV);
         }
         Vm_mV = VSpike_mV;
     }
